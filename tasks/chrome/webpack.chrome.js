d1a366b49b6a0589b5c97f5d2db4205b478108ae
Maxim Topchu <maximtop@gmail.com>
2019-08-08 20:30:38 +0300

    prepare manifest for publishing

--- tasks/chrome/webpack.chrome.js
+++ tasks/chrome/webpack.chrome.js
@@ -1,26 +1,37 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ZipWebpackPlugin = require('zip-webpack-plugin');
+const pJson = require('../../package');
 const common = require('../webpack.common');
 const { updateManifest } = require('../helpers');
 const chromeManifestDiff = require('./manifest.chrome');
 
 const CHROME_PATH = 'chrome';
 
+const plugins = [new CopyWebpackPlugin([
+    {
+        from: path.resolve(__dirname, '../manifest.common.json'),
+        to: 'manifest.json',
+        // eslint-disable-next-line no-shadow, no-unused-vars
+        transform: (content, path) => updateManifest(content, chromeManifestDiff),
+    },
+])];
+
+if (process.env.NODE_ENV === 'beta') {
+    plugins.push(
+        new ZipWebpackPlugin({
+            path: '../',
+            filename: `chrome-beta-${pJson.version}.zip`,
+        })
+    );
+}
+
 const chromeConfig = {
     output: {
         path: path.join(common.output.path, CHROME_PATH),
     },
-    plugins: [
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, '../manifest.common.json'),
-                to: 'manifest.json',
-                // eslint-disable-next-line no-shadow, no-unused-vars
-                transform: (content, path) => updateManifest(content, chromeManifestDiff),
-            },
-        ]),
-    ],
+    plugins,
 };
 
 module.exports = merge(common, chromeConfig);
