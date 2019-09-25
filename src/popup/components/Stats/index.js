a9e44db54e33ffc0bb5d5945b24f3db5aed489e8
Maxim Topchu <maximtop@gmail.com>
2019-09-25 15:43:44 +0300

    ADG-2498 retrive info immediately

--- src/popup/components/Stats/index.js
+++ src/popup/components/Stats/index.js
@@ -6,11 +6,25 @@ import './stats.pcss';
 const Stats = observer(() => {
     const { settingsStore } = useContext(rootStore);
 
-    useEffect(() => {
+    const updateStats = () => {
+        const UPDATE_INTERVAL = 1000;
+        // first time run immediately;
+        settingsStore.getProxyStats();
+
+        // next time once per second
         const intervalId = setInterval(async () => {
             await settingsStore.getProxyStats();
-        }, 1000);
-        return () => clearInterval(intervalId);
+        }, UPDATE_INTERVAL);
+
+        const onUnmount = () => {
+            clearInterval(intervalId);
+        };
+
+        return onUnmount;
+    };
+
+    useEffect(() => {
+        return updateStats();
     }, []);
 
     const { mbytesDownloaded = '...', downloadSpeedMbytesPerSec = '...' } = settingsStore.proxyStats || {};
