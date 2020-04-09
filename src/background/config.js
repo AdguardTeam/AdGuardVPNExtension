9ac0b7a040dccd3ebbbf597e924afeb8c19620c4
Maxim Topciu <maximtop@gmail.com>
2020-04-09 10:43:22 +0300

    AG-1739 make possible to login via social providers to the testauth

--- src/background/config.js
+++ src/background/config.js
@@ -1,17 +1,20 @@
 /* global PRODUCTION */
+import log from '../lib/logger';
+
 const ENVS = {
     PROD: 'prod',
-    DEV: 'dev',
+    TEST: 'test',
 };
 
 // Value of PRODUCTION variable is set by Webpack. For beta and prod we set it true,
 // otherwise we set it false
-const currentEnv = PRODUCTION ? ENVS.PROD : ENVS.DEV;
+const currentEnv = PRODUCTION ? ENVS.PROD : ENVS.TEST;
+log.debug(`Current staging: ${currentEnv}`);
 
 // Account section
 // API description - projects/ADGUARD/repos/adguard-account-service/browse
 const ACCOUNT_API_URLS_MAP = {
-    [ENVS.DEV]: 'https://testaccount.adguard.com/',
+    [ENVS.TEST]: 'https://testaccount.adguard.com/',
     [ENVS.PROD]: 'https://account.adguard.com/',
 };
 
@@ -20,7 +23,7 @@ export const ACCOUNT_API_URL = `${ACCOUNT_API_URLS_MAP[currentEnv]}api/1.0/`;
 // VPN section
 // API description - projects/ADGUARD/repos/adguard-vpn-backend-service/browse
 const VPN_API_URLS_MAP = {
-    [ENVS.DEV]: 'http://testapi.adguard.io/', // http - IMPORTANT!
+    [ENVS.TEST]: 'http://testapi.adguard.io/', // http - IMPORTANT!
     [ENVS.PROD]: 'https://api.adguard.io/',
 };
 
@@ -29,16 +32,21 @@ export const VPN_API_URL = `${VPN_API_URLS_MAP[currentEnv]}api/v1/`;
 // Auth section
 // API description - projects/ADGUARD/repos/adguard-auth-service/browse
 const AUTH_API_URLS_MAP = {
-    [ENVS.DEV]: 'https://testauth.adguard.com/',
+    [ENVS.TEST]: 'https://testauth.adguard.com/',
     [ENVS.PROD]: 'https://auth.adguard.com/',
 };
 
-export const AUTH_API_URL = `${AUTH_API_URLS_MAP[currentEnv]}`;
+export const AUTH_API_URL = AUTH_API_URLS_MAP[currentEnv];
 export const AUTH_BASE_URL = `${AUTH_API_URL}oauth/authorize`;
 export const AUTH_REDIRECT_URI = `${AUTH_API_URL}oauth.html`;
 export const AUTH_ACCESS_TOKEN_KEY = 'auth.access.token';
 export const AUTH_CLIENT_ID = 'adguard-vpn-extension';
-export const PASSWORD_RECOVERY_URL = 'https://adguard-vpn.com/forward.html?action=recovery_password&from=popup&app=vpn_extension';
+// TODO should I add api links to the tds?
+// export const PASSWORD_RECOVERY_URL = 'https://adguard-vpn.com/forward.html?action=recovery_password&from=popup&app=vpn_extension';
+export const PASSWORD_RECOVERY_URL = `${AUTH_API_URL}account/recovery_password.html`;
+// export const EDIT_ACCOUNT_URL = 'https://adguard-vpn.com/forward.html?action=account_settings&from=options_screen&app=vpn_extension';
+export const EDIT_ACCOUNT_URL = `${AUTH_API_URL}login.html`;
+
 
 // Websocket
 export const WS_API_URL_TEMPLATE = 'wss://{{host}}:8443/user_metrics';
@@ -57,7 +65,6 @@ export const POPUP_FEEDBACK_URL = 'https://adguard-vpn.com/forward.html?action=f
 export const POPUP_DEFAULT_SUPPORT_URL = 'https://adguard-vpn.com/forward.html?action=support&from=popup&app=vpn_extension';
 
 // Options page
-export const EDIT_ACCOUNT_URL = 'https://adguard-vpn.com/forward.html?action=account_settings&from=options_screen&app=vpn_extension';
 export const WEBSITE_URL = 'https://adguard-vpn.com/forward.html?action=adguard_site&from=options_screen&app=vpn_extension';
 export const STORE_URL = 'https://adguard-vpn.com/forward.html?action=store&from=options_screen&app=vpn_extension';
 export const FEEDBACK_URL = 'https://adguard-vpn.com/forward.html?action=feedback&from=options_screen&app=vpn_extension';
