b90a51988a13cb107010ab489418f75f6ba71213
Maxim Topciu <maximtop@gmail.com>
2020-05-22 17:24:55 +0300

    AG-2464 revert production mode

--- tasks/webpack.common.js
+++ tasks/webpack.common.js
@@ -71,8 +71,7 @@ const config = {
     plugins: [
         // Define environment for choosing appropriate api urls
         new webpack.DefinePlugin({
-            // PRODUCTION: JSON.stringify(!IS_DEV), // TODO uncomment
-            PRODUCTION: JSON.stringify(true), // TODO remove
+            PRODUCTION: JSON.stringify(!IS_DEV),
         }),
         new webpack.NormalModuleReplacementPlugin(/\.\/abstractProxyApi/, ((resource) => {
             if (process.env.BROWSER === 'firefox') {
