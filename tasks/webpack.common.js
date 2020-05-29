bca6e1255c68971f9b42eff5a7aea5100df9f03b
Maxim Topciu <maximtop@gmail.com>
2020-05-29 14:55:02 +0300

    AG-2579 display all endpoints for testers

--- tasks/webpack.common.js
+++ tasks/webpack.common.js
@@ -71,7 +71,8 @@ const config = {
     plugins: [
         // Define environment for choosing appropriate api urls
         new webpack.DefinePlugin({
-            PRODUCTION: JSON.stringify(!IS_DEV),
+            PRODUCTION: JSON.stringify(true), // TODO remove
+            // PRODUCTION: JSON.stringify(!IS_DEV), // TODO uncomment after test
             BROWSER: JSON.stringify(process.env.BROWSER),
         }),
         new webpack.NormalModuleReplacementPlugin(/\.\/abstractProxyApi/, ((resource) => {
