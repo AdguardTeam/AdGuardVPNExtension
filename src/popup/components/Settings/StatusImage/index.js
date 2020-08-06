9cc5fdc31983188d81aed86785ca07e71c16c4cc
IVAN K <i.kulyev@adguard.com>
2020-08-06 13:04:26 +0300

    fix/AG-3437 add endpoint border, fix img status

--- src/popup/components/Settings/StatusImage/index.js
+++ src/popup/components/Settings/StatusImage/index.js
@@ -14,13 +14,14 @@ const StatusImage = observer(() => {
         isDisconnectedRetrying,
         isConnectingRetrying,
         isConnectingIdle,
+        canBeExcluded,
     } = settingsStore;
 
     const statusClassName = classnames(
         'status-image',
         { 'status-image--enabled': isConnected },
         { 'status-image--connecting': isConnectingIdle || isDisconnectedRetrying },
-        { 'status-image--exclusions-disable': displayExlusionScreen },
+        { 'status-image--exclusions-disable': displayExlusionScreen && canBeExcluded },
         { 'status-image--server-error': isDisconnectedRetrying || isConnectingRetrying }
     );
 
