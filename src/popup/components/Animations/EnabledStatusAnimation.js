940d494c92aa96f88726d5db5e997b10c01daefc
Ivan Kulyev <i.kulyev@adguard.com>
2020-08-17 11:02:54 +0300

    Pull request #207: fix/AG-3655 animation bush / fix lock img position
    Merge in VPN/browser-extension from fix/AG-3655 to master
    
    Squashed commit of the following:
    
    commit f8c0554a0eb073e9e49f95a5927772352e2efb13
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Aug 14 17:04:23 2020 +0300
    
        fix/AG-3655 update animation
    
    commit bc95be459390e44aea7d4fb18c66f7528ee9d8ba
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Aug 14 15:29:37 2020 +0300
    
        fix/AG-3655 fix names
    
    commit 6fd1ab9a868cb89e43a1fc325e8a95b0b9e08ef6
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Aug 14 15:23:01 2020 +0300
    
        fix/AG-3655 fix animation name
    
    commit 8f0b559d83ebc0a00fe5c65b4ceadeac9816725f
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Aug 14 14:59:44 2020 +0300
    
        fix/AG-3655 animation bush / fix lock img position
    
    commit 75f514466748d17e2a6429638c94084a2436c08d
    Merge: dd18fc3 c775c2e
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Aug 14 14:07:06 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension into master
    
    commit dd18fc3b1f219cbd49cbbb434de16efb2e339dd1
    Merge: a422abb 5df0ae7
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Mon Aug 10 13:58:03 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension into master
    
    commit a422abb988c9478d6b90b63c806dee12bdbf48a7
    Merge: ad569b9 249e160
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Thu Jul 30 10:05:22 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit ad569b97e6b71479a7f1a8430efb9dab71cd46d6
    Merge: 8ca59e8 e7dbb58
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Mon Jul 27 16:31:44 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 8ca59e86bb35b8d095f29a3023134858a861fa17
    Merge: 86c481c 658e6b5
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Mon Jul 20 17:20:24 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 86c481c6d8e488c0cf6a66e7996e26b2e4615131
    Merge: 2d9a3f7 26985dc
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Jul 17 15:01:08 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 2d9a3f7422f3eb4a78ddf86a6c5113040bc6de75
    Merge: 7c702a0 f763cc4
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Thu Jul 16 11:24:42 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 7c702a0d7ff5a0796cb710a16175fc21a7a713bd
    Merge: 41f57c2 3f98b6b
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Jul 10 16:38:33 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 41f57c29c8c846c11a4b5fb82d365ca78512b152
    Merge: 0dede0b 3024adc
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Fri Jul 10 11:06:05 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 0dede0bc33d4b80557196744f625bd2eb43b1b02
    Merge: 75af51f 5b2f165
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Thu Jul 9 16:07:40 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 75af51f596aa11d02a3bc4fdd6f81c4c3dde32ca
    Merge: 05d85cb 0792a12
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Mon Jul 6 10:29:18 2020 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/vpn/browser-extension
    
    commit 05d85cbc51203f7da14f8b5c07e4e092044c4a59
    Author: IVAN K <i.kulyev@adguard.com>
    Date:   Tue Jun 16 11:27:18 2020 +0300
    
        AG-2812 letter-spacing fix in 2fa

---
+++ src/popup/components/Animations/EnabledStatusAnimation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from './enabledStatusAnimation.json';
+
+function EnabledStatusAnimation() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    };
+
+    return <Lottie options={defaultOptions} width={200} height={198} />;
+}
+
+export default EnabledStatusAnimation;
