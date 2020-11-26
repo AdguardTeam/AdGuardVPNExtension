edd765df509ffd92e5c0cf2c4884ce7ff3da98e6
Maxim Topciu <maximtop@gmail.com>
2020-11-26 15:17:31 +0200

    AG-5283 fix edge store and edge feedback urls

--- tasks/appConfig.js
+++ tasks/appConfig.js
@@ -14,14 +14,10 @@ const BROWSER_CONF = {
         FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_firefox&from=options_screen&app=vpn_extension',
     },
     [BROWSERS.EDGE]: {
-        // FIXME change url after getting it from the store
-        POPUP_STORE_URL: 'https://adguard-vpn.com/forward.html?action=firefox_store&from=popup&app=vpn_extension',
-        // FIXME get edge feedback url from @denis
-        POPUP_FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_firefox&from=popup&app=vpn_extension',
-        // FIXME change url after getting it from the store
-        OPTIONS_STORE_URL: 'https://adguard-vpn.com/forward.html?action=firefox_store&from=options_screen&app=vpn_extension',
-        // FIXME get edge feedback url from @denis
-        FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_firefox&from=options_screen&app=vpn_extension',
+        POPUP_STORE_URL: 'https://adguard-vpn.com/forward.html?action=edge_store&from=popup&app=vpn_extension',
+        POPUP_FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_edge&from=popup&app=vpn_extension',
+        OPTIONS_STORE_URL: 'https://adguard-vpn.com/forward.html?action=edge_store&from=options_screen&app=vpn_extension',
+        FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_edge&from=options_screen&app=vpn_extension',
     },
 };
 
