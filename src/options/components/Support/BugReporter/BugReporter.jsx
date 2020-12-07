29602403e4551ffae3a32caaadb9042157551f19
Maxim Topciu <maximtop@gmail.com>
2020-12-07 13:07:51 +0200

    AG-4962 manage state of form

--- src/options/components/Support/BugReporter/BugReporter.jsx
+++ src/options/components/Support/BugReporter/BugReporter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { reactTranslator } from '../../../../reactCommon/reactTranslator';
 import { Title } from '../../ui/Title';
@@ -8,34 +8,79 @@ import './bug-report.pcss';
 // FIXME add back button
 // FIXME fill email input
 export const BugReporter = ({ closeHandler }) => {
+    const [formState, setFormState] = useState({
+        email: '',
+        bug_description: '',
+    });
+
     const handleSubmit = (e) => {
-        console.log(e.target.value);
+        e.preventDefault();
+        console.log(formState);
     };
+
+    const formChangeHandler = (e) => {
+        const { id, value } = e.target;
+
+        setFormState((prevState) => {
+            return {
+                ...prevState,
+                [id]: value,
+            };
+        });
+    };
+
+    console.log(formState);
 
     return (
         <>
-            <button onClick={closeHandler}>Back</button>
+            <button type="button" onClick={closeHandler}>Back</button>
+
             <Title title={reactTranslator.translate('options_report_bug_title')} />
+
             <div className="bug-report">
                 <div className="bug-report__description">
                     {reactTranslator.translate('options_bug_report_page_description')}
                 </div>
-                <form className="bug-report__form" onSubmit={handleSubmit}>
-                    <label className="bug-report__label" htmlFor="email">Email</label>
-                    <input type="email" placeholder="example@mail.com" />
-                    <label className="bug-report__label" htmlFor="bug-report">
+                <form
+                    className="bug-report__form"
+                    onSubmit={handleSubmit}
+                    onChange={formChangeHandler}
+                >
+                    <label
+                        className="bug-report__label"
+                        htmlFor="email"
+                    >
+                        {reactTranslator.translate('options_bug_report_email_label')}
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="example@mail.com"
+                        defaultValue={formState.email}
+                    />
+
+                    <label
+                        className="bug-report__label"
+                        htmlFor="bug_report_description"
+                    >
                         {reactTranslator.translate('options_bug_report_textarea_label')}
                     </label>
                     <textarea
-                        aria-placeholder={reactTranslator.translate('options_bug_report_textarea_placeholder')}
-                        name="bug-report"
-                        id="bug-report"
-                        cols="30"
-                        rows="10"
-                        placeholder="Please enter your message"
+                        name="bug_description"
+                        id="bug_description"
+                        placeholder={reactTranslator.translate('options_bug_report_textarea_placeholder')}
+                        defaultValue={formState.bug_description}
                     />
 
-                    <button>Send</button>
+                    {/* FIXME disable button if form is empty */}
+                    {/* FIXME add sending state to the button */}
+                    {/* FIXME handle network connection errors */}
+                    <button
+                        type="submit"
+                        className="button button--medium button--primary bug-report__action"
+                    >
+                        {reactTranslator.translate('options_bug_report_send_button')}
+                    </button>
                 </form>
             </div>
         </>
