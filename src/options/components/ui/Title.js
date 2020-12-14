74065e4ef20930a9beb37060fb5af6180f7c4903
Maxim Topciu <maximtop@gmail.com>
2020-12-14 19:11:24 +0200

    AG-4962 remove suggest a feture url from title

--- src/options/components/ui/Title.js
+++ src/options/components/ui/Title.js
@@ -1,19 +1,7 @@
 import React from 'react';
 
-import { SUGGEST_FEATURE } from '../../../background/config';
-import { reactTranslator } from '../../../reactCommon/reactTranslator';
-
 export const Title = ({ title }) => (
     <h2 className="content__title">
         {title}
-
-        <a
-            href={SUGGEST_FEATURE}
-            className="content__link"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            {reactTranslator.translate('suggest_feature')}
-        </a>
     </h2>
 );
