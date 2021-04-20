44758c15324190ca180b6cea6cc9e10ddef7d2e3
Maxim Topciu <maximtop@gmail.com>
2021-04-20 09:54:42 +0300

    use WEBSITE_DOMAIN to split prod and test builds

--- tasks/appConfig.js
+++ tasks/appConfig.js
@@ -1,33 +1,34 @@
-const { STAGE_ENVS } = require('./consts');
 const { BROWSERS, ENVS } = require('./consts');
+
+const { WEBSITE_DOMAIN } = process.env;
 
 const URLS_MAP_RELEASE = {
     [BROWSERS.CHROME]: {
-        POPUP_STORE_URL: 'https://adguard-vpn.com/forward.html?action=chrome_store&from=popup&app=vpn_extension',
-        POPUP_FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_chrome&from=popup&app=vpn_extension',
-        OPTIONS_STORE_URL: 'https://adguard-vpn.com/forward.html?action=chrome_store&from=options_screen&app=vpn_extension',
-        FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_chrome&from=options_screen&app=vpn_extension',
+        POPUP_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=chrome_store&from=popup&app=vpn_extension`,
+        POPUP_FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_chrome&from=popup&app=vpn_extension`,
+        OPTIONS_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=chrome_store&from=options_screen&app=vpn_extension`,
+        FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_chrome&from=options_screen&app=vpn_extension`,
     },
     [BROWSERS.FIREFOX]: {
-        POPUP_STORE_URL: 'https://adguard-vpn.com/forward.html?action=firefox_store&from=popup&app=vpn_extension',
-        POPUP_FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_firefox&from=popup&app=vpn_extension',
-        OPTIONS_STORE_URL: 'https://adguard-vpn.com/forward.html?action=firefox_store&from=options_screen&app=vpn_extension',
-        FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_firefox&from=options_screen&app=vpn_extension',
+        POPUP_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=firefox_store&from=popup&app=vpn_extension`,
+        POPUP_FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_firefox&from=popup&app=vpn_extension`,
+        OPTIONS_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=firefox_store&from=options_screen&app=vpn_extension`,
+        FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_firefox&from=options_screen&app=vpn_extension`,
     },
     [BROWSERS.EDGE]: {
-        POPUP_STORE_URL: 'https://adguard-vpn.com/forward.html?action=edge_store&from=popup&app=vpn_extension',
-        POPUP_FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_edge&from=popup&app=vpn_extension',
-        OPTIONS_STORE_URL: 'https://adguard-vpn.com/forward.html?action=edge_store&from=options_screen&app=vpn_extension',
-        FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_edge&from=options_screen&app=vpn_extension',
+        POPUP_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=edge_store&from=popup&app=vpn_extension`,
+        POPUP_FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_edge&from=popup&app=vpn_extension`,
+        OPTIONS_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=edge_store&from=options_screen&app=vpn_extension`,
+        FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_edge&from=options_screen&app=vpn_extension`,
     },
 };
 
 const URLS_MAP_BETA = {
     [BROWSERS.CHROME]: {
-        POPUP_STORE_URL: 'https://adguard-vpn.com/forward.html?action=chrome_store_beta&from=popup&app=vpn_extension',
-        OPTIONS_STORE_URL: 'https://adguard-vpn.com/forward.html?action=chrome_store_beta&from=options_screen&app=vpn_extension',
-        POPUP_FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_chrome&from=popup&app=vpn_extension',
-        FEEDBACK_URL: 'https://adguard-vpn.com/forward.html?action=feedback_chrome&from=options_screen&app=vpn_extension',
+        POPUP_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=chrome_store_beta&from=popup&app=vpn_extension`,
+        OPTIONS_STORE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=chrome_store_beta&from=options_screen&app=vpn_extension`,
+        POPUP_FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_chrome&from=popup&app=vpn_extension`,
+        FEEDBACK_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=feedback_chrome&from=options_screen&app=vpn_extension`,
     },
 };
 
@@ -36,39 +37,32 @@ const URLS_MAP = {
     [ENVS.BETA]: { ...URLS_MAP_RELEASE, ...URLS_MAP_BETA },
 };
 
-// Account section API description - projects/ADGUARD/repos/adguard-account-service/browse
 // VPN section API description - projects/ADGUARD/repos/adguard-vpn-backend-service/browse
 // Auth section API description - projects/ADGUARD/repos/adguard-auth-service/browse
-const STAGE_CONF_BY_ENV = {
+const STAGE_CONF = {
     VPN_API_URL: process.env.VPN_API_URL,
     AUTH_API_URL: process.env.AUTH_API_URL,
-};
-
-const getStageConf = (stageEnv) => {
-    const prefix = stageEnv === STAGE_ENVS.TEST ? 'dev.' : '';
-    return {
-        ...STAGE_CONF_BY_ENV,
-        PASSWORD_RECOVERY_URL: `${prefix}adguard-vpn.com/forward.html?action=recovery_password&from=popup&app=vpn_extension`,
-        EDIT_ACCOUNT_URL: `${prefix}adguard-vpn.com/forward.html?action=account_settings&from=options_screen&app=vpn_extension`,
-    };
 };
 
 const COMMON = {
     // Websocket
     WS_API_URL_TEMPLATE: 'wss://{{host}}:443/user?hash={{hash}}',
     // Privacy and EULA
-    PRIVACY_URL: 'https://adguard-vpn.com/forward.html?action=privacy&from=popup&app=vpn_extension',
-    EULA_URL: 'https://adguard-vpn.com/forward.html?action=eula&from=popup&app=vpn_extension',
+    PRIVACY_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=privacy&from=popup&app=vpn_extension`,
+    EULA_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=eula&from=popup&app=vpn_extension`,
     // Commercial
-    OTHER_PRODUCTS_URL: 'https://adguard-vpn.com/forward.html?action=other_products&from=popup&app=vpn_extension',
+    OTHER_PRODUCTS_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=other_products&from=popup&app=vpn_extension`,
     // Support
-    POPUP_DEFAULT_SUPPORT_URL: 'https://adguard-vpn.com/forward.html?action=support&from=popup&app=vpn_extension',
+    POPUP_DEFAULT_SUPPORT_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=support&from=popup&app=vpn_extension`,
     // Options page
-    WEBSITE_URL: 'https://adguard-vpn.com/forward.html?action=adguard_site&from=options_screen&app=vpn_extension',
-    FAQ_URL: 'https://adguard-vpn.com/forward.html?action=faq&from=options_screen&app=vpn_extension',
-    DISCUSS_URL: 'https://adguard-vpn.com/forward.html?action=discuss&from=options_screen&app=vpn_extension',
-    SUGGEST_FEATURE: 'https://adguard-vpn.com/forward.html?action=suggest_feature&from=options_screen&app=vpn_extension',
-    THANK_YOU_PAGE_URL: 'https://adguard-vpn.com/forward.html?action=thank_you&from=background_page&app=vpn_extension',
+    WEBSITE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=adguard_site&from=options_screen&app=vpn_extension`,
+    FAQ_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=faq&from=options_screen&app=vpn_extension`,
+    DISCUSS_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=discuss&from=options_screen&app=vpn_extension`,
+    SUGGEST_FEATURE: `https://${WEBSITE_DOMAIN}/forward.html?action=suggest_feature&from=options_screen&app=vpn_extension`,
+    THANK_YOU_PAGE_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=thank_you&from=background_page&app=vpn_extension`,
+    PASSWORD_RECOVERY_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=recovery_password&from=popup&app=vpn_extension`,
+    EDIT_ACCOUNT_URL: `https://${WEBSITE_DOMAIN}/forward.html?action=account_settings&from=options_screen&app=vpn_extension`,
+    // API
     AUTH_ACCESS_TOKEN_KEY: 'auth.access.token',
     AUTH_CLIENT_ID: 'adguard-vpn-extension',
 };
@@ -86,7 +80,7 @@ const genAppConfig = (browser, stageEnv, buildingEnv) => {
         BUILD_ENV: buildingEnv,
         STAGE_ENV: stageEnv,
         ...browserConf,
-        ...getStageConf(stageEnv),
+        ...STAGE_CONF,
         ...COMMON,
     };
 };
