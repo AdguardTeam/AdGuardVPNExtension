9de79edc50817172c60ba9c77248e1753228fe26
tvinzz <o.labudko@adguard.com>
2021-08-11 13:11:43 +0300

    fixes

--- src/options/components/SignedOut/index.js
+++ src/options/components/SignedOut/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import './signedout.pcss';
 import { reactTranslator } from '../../../common/reactTranslator';
 
-const SignedOut = () => {
+import './signedout.pcss';
+
+export const SignedOut = () => {
     return (
         <div className="signedout">
             <div className="signedout__ninja-image" />
             <div className="signedout__title">
-                {reactTranslator.getMessage('signedout_page_title')}
+                {reactTranslator.getMessage('options_signedout_page_title')}
             </div>
             <div className="signedout__description">
-                {reactTranslator.getMessage('signedout_page_description')}
+                {reactTranslator.getMessage('options_signedout_page_description')}
             </div>
         </div>
     );
-};
-
-export {
-    SignedOut,
 };
