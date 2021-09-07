c09617e6feba45a10ba67afb7d663656fba37647
tvinzz <o.labudko@adguard.com>
2021-09-07 12:50:45 +0300

    added test for problematic user email

--- tests/background/actions.test.js
+++ tests/background/actions.test.js
@@ -18,4 +18,13 @@ describe('Actions tests', () => {
         credentials.getUsername.mockClear();
         expect(url).toEqual(expectedUrl);
     });
+
+    it('Test email with special symbols', async () => {
+        credentials.getUsername.mockImplementation(() => 'tester+000@test.com');
+
+        const expectedUrl = 'https://adguard-vpn.com/license.html?action=upgrade_license&email=tester%2B000%40test.com';
+        const url = await actions.getPremiumPromoPageUrl();
+        credentials.getUsername.mockClear();
+        expect(url).toEqual(expectedUrl);
+    });
 });
