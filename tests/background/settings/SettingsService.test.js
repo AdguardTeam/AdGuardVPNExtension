68240157a906f0192232b9a6c15a7f7492bffa82
tvinzz <o.labudko@adguard.com>
2021-09-23 04:03:47 +0300

    fixed tests

--- tests/background/settings/SettingsService.test.js
+++ tests/background/settings/SettingsService.test.js
@@ -28,7 +28,7 @@ let settingsService;
 describe('SettingsService', () => {
     describe('init', () => {
         const expectedSettings = {
-            VERSION: '8',
+            VERSION: '9',
             enabled: true,
             showPromo: true,
         };
@@ -62,7 +62,7 @@ describe('SettingsService', () => {
         });
 
         it('inits correctly if versions do not match', async () => {
-            const unmatchedVersion = '9';
+            const unmatchedVersion = '10';
             storage.set(settingsService.SETTINGS_KEY, {
                 ...expectedSettings,
                 VERSION: unmatchedVersion,
@@ -70,7 +70,7 @@ describe('SettingsService', () => {
             await settingsService.init();
             const settings = settingsService.getSettings();
             expect(settings.VERSION)
-                .toBe('8');
+                .toBe('9');
             expect(settings.enabled)
                 .toBe(defaults.enabled);
             expect(settings.showPromo)
