6de91bd694c1aa587a858682602b1310674bedb7
Oleg Labudko <o.labudko@adguard.com>
2021-10-12 13:48:40 +0300

    Pull request: AG-10430 Refactor getOptionsData
    Merge in EXTENSIONS/vpn-extension from feature/AG-10430 to master
    
    Squashed commit of the following:
    
    commit 9e0b70d703d60a637674bf6bc72aaf99812d3aec
    Merge: 3daf4a83 8f9cb6ad
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Oct 12 12:37:15 2021 +0300
    
        Merge branch 'master' into feature/AG-10430
    
    commit 3daf4a8353641243f4f09d3ca3fd8bb418a228b9
    Merge: 73cd2e0b 94f11638
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Oct 12 12:00:15 2021 +0300
    
        Merge branch 'master' into feature/AG-10430
    
    commit 73cd2e0b928f6265b77c3cc564c5d62b10dc4013
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Oct 12 00:43:33 2021 +0300
    
        fixed username issue on relogin
    
    commit 936b9dca11e71fe4356a83f511070315129e0ff8
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Mon Oct 11 14:00:49 2021 +0300
    
        removed class OptionsData and added func getOptionsData to messageHandler
    
    commit 4e2d5b3609e3a5d410cbad31f2d82bf1aa91709f
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Mon Oct 11 13:47:45 2021 +0300
    
        removed redundant receivedAuthenticationInfo
    
    commit 45aae0879830b5ba2b85bc2c37766e9f9f1a973b
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Oct 7 02:01:35 2021 +0300
    
        refactored authStore
    
    commit a78c70c1fc286e9baa9e3cb5864bec3389448a37
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Oct 7 01:47:01 2021 +0300
    
        added class OptionsData
    
    commit 2b85ac36d726221a54b5af02735ba91619a36aec
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Oct 7 01:19:42 2021 +0300
    
        refactored options data

--- src/background/messageHandler.js
+++ src/background/messageHandler.js
@@ -1,6 +1,6 @@
 import browser from 'webextension-polyfill';
 
-import { MESSAGES_TYPES } from '../lib/constants';
+import { MESSAGES_TYPES, SETTINGS_IDS } from '../lib/constants';
 import auth from './auth';
 import popupData from './popupData';
 import endpoints from './endpoints';
@@ -23,6 +23,44 @@ import { logStorage } from '../lib/log-storage';
 import { setDesktopVpnEnabled } from './connectivity/connectivityService/connectivityFSM';
 
 const eventListeners = {};
+
+const getOptionsData = async () => {
+    const appVersion = appStatus.version;
+    const username = await credentials.getUsername();
+    const isRateVisible = settings.getSetting(SETTINGS_IDS.RATE_SHOW);
+    const webRTCEnabled = settings.getSetting(SETTINGS_IDS.HANDLE_WEBRTC_ENABLED);
+    const contextMenusEnabled = settings.getSetting(SETTINGS_IDS.CONTEXT_MENU_ENABLED);
+    const helpUsImprove = settings.getSetting(SETTINGS_IDS.HELP_US_IMPROVE);
+    const dnsServer = settings.getSetting(SETTINGS_IDS.SELECTED_DNS_SERVER);
+    const appearanceTheme = settings.getSetting(SETTINGS_IDS.APPEARANCE_THEME);
+
+    const regularExclusions = exclusions.regular?.getExclusionsList();
+    const selectiveExclusions = exclusions.selective?.getExclusionsList();
+    const exclusionsCurrentMode = exclusions.current?.mode;
+
+    const exclusionsData = {
+        regular: regularExclusions,
+        selective: selectiveExclusions,
+        currentMode: exclusionsCurrentMode,
+    };
+
+    const isAuthenticated = await auth.isAuthenticated();
+    // AG-644 set current endpoint in order to avoid bug in permissions checker
+    await endpoints.getSelectedLocation();
+
+    return {
+        appVersion,
+        username,
+        isRateVisible,
+        webRTCEnabled,
+        contextMenusEnabled,
+        helpUsImprove,
+        dnsServer,
+        appearanceTheme,
+        exclusionsData,
+        isAuthenticated,
+    };
+};
 
 const messageHandler = async (message, sender) => {
     const { type, data } = message;
@@ -60,6 +98,9 @@ const messageHandler = async (message, sender) => {
         case MESSAGES_TYPES.GET_POPUP_DATA: {
             const { url, numberOfTries } = data;
             return popupData.getPopupDataRetry(url, numberOfTries);
+        }
+        case MESSAGES_TYPES.GET_OPTIONS_DATA: {
+            return getOptionsData();
         }
         case MESSAGES_TYPES.GET_VPN_FAILURE_PAGE: {
             return endpoints.getVpnFailurePage();
@@ -204,9 +245,6 @@ const messageHandler = async (message, sender) => {
         case MESSAGES_TYPES.GET_SETTING_VALUE: {
             const { settingId } = data;
             return settings.getSetting(settingId);
-        }
-        case MESSAGES_TYPES.GET_APP_VERSION: {
-            return appStatus.version;
         }
         case MESSAGES_TYPES.SET_SETTING_VALUE: {
             const { settingId, value } = data;
--- src/options/components/App/App.jsx
+++ src/options/components/App/App.jsx
@@ -101,10 +101,12 @@ export const App = observer(() => {
                         }
                         case notifier.types.USER_AUTHENTICATED: {
                             authStore.setIsAuthenticated(true);
+                            await settingsStore.updateCurrentUsername();
                             break;
                         }
                         case notifier.types.USER_DEAUTHENTICATED: {
                             authStore.setIsAuthenticated(false);
+                            await settingsStore.updateCurrentUsername();
                             break;
                         }
                         default: {
