4231245d1aab1bb9cd224275d34fd30d5b156b99
tvinzz <o.labudko@adguard.com>
2021-12-02 23:51:22 +0300

    show message if no exclusions found

--- src/options/components/Exclusions/List/List.tsx
+++ src/options/components/Exclusions/List/List.tsx
@@ -3,9 +3,18 @@ import { observer } from 'mobx-react';
 
 import { ListItem } from './ListItem';
 import { rootStore } from '../../../stores';
+import {reactTranslator} from '../../../../common/reactTranslator';
 
 export const List = observer(() => {
     const { exclusionsStore } = useContext(rootStore);
+
+    if (exclusionsStore.exclusionsSearchValue && !exclusionsStore.preparedExclusions.length) {
+        return (
+            <div className="search__nothing-found">
+                {reactTranslator.getMessage('settings_exclusion_nothing_found')}
+            </div>
+        )
+    }
 
     return (
         <ul>
