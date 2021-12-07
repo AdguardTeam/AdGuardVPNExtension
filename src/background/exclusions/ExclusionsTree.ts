b07c55b6c3d037d2cd349d61a19609842161c600
Maxim Topciu <mtopciu@adguard.com>
2021-12-07 15:50:33 +0300

    AG-10618 add exclusions tree

---
+++ src/background/exclusions/ExclusionsTree.ts
@@ -0,0 +1,180 @@
+/* eslint-disable max-classes-per-file,no-continue */
+import { ExclusionStates } from '../../common/exclusionsConstants';
+
+class ExclusionDto {
+    id: string;
+
+    value: string;
+
+    state: ExclusionStates;
+
+    children: ExclusionDto[];
+
+    constructor(
+        id: string,
+        value: string,
+        state: ExclusionStates,
+        children: ExclusionDto[],
+    ) {
+        this.id = id;
+        this.value = value;
+        this.state = state;
+        this.children = children;
+    }
+}
+
+class ExclusionNode {
+    id: string;
+
+    value: string;
+
+    state: ExclusionStates;
+
+    // TODO add icon url
+    // iconUrl?: string;
+
+    children: ExclusionNode[];
+
+    constructor(id: string, value: string, state: ExclusionStates = ExclusionStates.Enabled) {
+        this.id = id;
+        this.value = value;
+        this.state = state;
+        this.children = [];
+    }
+
+    hasChildren() {
+        return this.children.length > 0;
+    }
+
+    addChild(child: ExclusionNode) {
+        this.children.push(child);
+    }
+
+    removeChild(id: string) {
+        const foundChild = this.children.find((child) => child.id === id);
+        if (foundChild) {
+            this.children = this.children.filter((child) => child.id !== foundChild.id);
+        } else {
+            for (let i = 0; i < this.children.length; i += 1) {
+                const child = this.children[i];
+                child.removeChild(id);
+            }
+        }
+        // FIXME: if this.children length === 0, remove parents to root
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    calculateState(children: ExclusionNode[]): ExclusionStates {
+        return ExclusionStates.Enabled;
+    }
+
+    getState(): ExclusionStates {
+        if (this.hasChildren()) {
+            return this.calculateState(this.children);
+        }
+        return this.state;
+    }
+
+    serialize(): ExclusionDto {
+        const children = this.children.map((child) => child.serialize());
+        return new ExclusionDto(this.id, this.value, this.getState(), children);
+    }
+}
+
+interface ExclusionInterface {
+    id: string,
+    hostname: string,
+    state: ExclusionStates,
+}
+
+interface GroupsInterface {
+    id: string,
+    value: string,
+}
+
+interface ServiceInterface {
+    id: string,
+    name: string,
+    groups: GroupsInterface[],
+}
+
+interface ServicesInterface {
+    [id: string]: ServiceInterface,
+}
+
+interface IndexedServicesInterface {
+    [id: string]: string | ServicesInterface
+    services: ServicesInterface,
+}
+
+const getTld = (hostname: string) => {
+    return hostname.replace('*.', '');
+};
+
+export class ExclusionsTree {
+    exclusionsTree = new ExclusionNode('root', 'root');
+
+    groupIndex: { [key: string]: ExclusionNode } = {};
+
+    exclusions: ExclusionInterface[];
+
+    services: IndexedServicesInterface;
+
+    constructor(
+        exclusions: ExclusionInterface[] = [],
+        services: IndexedServicesInterface = { services: {} },
+    ) {
+        this.exclusions = exclusions;
+        this.services = services;
+    }
+
+    generateTree() {
+        for (let i = 0; i < this.exclusions.length; i += 1) {
+            const exclusion = this.exclusions[i];
+            const hostnameTld = getTld(exclusion.hostname);
+
+            if (this.groupIndex[hostnameTld]) {
+                const exclusionNode = new ExclusionNode(exclusion.id, exclusion.hostname);
+                this.groupIndex[hostnameTld].addChild(exclusionNode);
+                continue;
+            }
+
+            if (this.services[hostnameTld]) {
+                const id = this.services[hostnameTld] as string;
+                const service = this.services.services[id];
+                const serviceNode = new ExclusionNode(service.id, service.name);
+
+                const group = service.groups.find((group) => group.value === hostnameTld);
+
+                if (!group) {
+                    // TODO add exclusion outside of service
+                    continue;
+                }
+
+                const groupNode = new ExclusionNode(group.id, group.value);
+                this.groupIndex[hostnameTld] = groupNode;
+
+                const exclusionNode = new ExclusionNode(exclusion.id, exclusion.hostname);
+
+                groupNode.addChild(exclusionNode);
+                serviceNode.addChild(groupNode);
+
+                this.exclusionsTree.addChild(serviceNode);
+
+                continue;
+            }
+
+            // if not in the service
+            // remove _group from interpolation, temp solution
+            const groupNode = new ExclusionNode(hostnameTld, hostnameTld);
+            this.groupIndex[hostnameTld] = groupNode;
+            const exclusionNode = new ExclusionNode(exclusion.id, exclusion.hostname);
+            groupNode.addChild(exclusionNode);
+            this.exclusionsTree.addChild(groupNode);
+        }
+    }
+
+    getExclusions() {
+        return this.exclusionsTree.serialize();
+    }
+}
---
+++ tests/background/exclusions/ExclusionsTree.test.ts
@@ -0,0 +1,158 @@
+import { nanoid } from 'nanoid';
+
+import { ExclusionsTree } from '../../../src/background/exclusions/ExclusionsTree';
+import { ExclusionStates } from '../../../src/common/exclusionsConstants';
+import clearAllMocks = jest.clearAllMocks;
+
+jest.mock('nanoid');
+
+const nanoidMock = nanoid as jest.MockedFunction<() => string>;
+nanoidMock.mockImplementation((() => {
+    let counter = 0;
+    return () => {
+        const currentId = counter;
+        counter += 1;
+        return currentId.toString();
+    };
+})());
+
+describe('ExclusionsTree', () => {
+    afterEach(() => {
+        clearAllMocks();
+    });
+
+    it('generate exclusions which are in the service', () => {
+        const exclusions = [
+            { id: '1', hostname: 'example.org', state: ExclusionStates.Enabled },
+            { id: '2', hostname: '*.example.org', state: ExclusionStates.Enabled },
+        ];
+
+        const services = {
+            services: {
+                '1_service': {
+                    id: '1_service',
+                    name: 'example',
+                    groups: [
+                        {
+                            id: '1_group',
+                            value: 'example.org',
+                        },
+                        {
+                            id: '2_group',
+                            value: 'example.net',
+                        },
+                    ],
+                },
+            },
+            'example.org': '1_service',
+            '*.example.org': '1_service',
+        };
+
+        const exclusionsTree = new ExclusionsTree(exclusions, services);
+
+        exclusionsTree.generateTree();
+
+        const exclusionsData = exclusionsTree.getExclusions();
+
+        expect(exclusionsData).toEqual({
+            id: 'root',
+            value: 'root',
+            state: ExclusionStates.Enabled,
+            children: [
+                {
+                    id: '1_service',
+                    value: 'example',
+                    state: ExclusionStates.Enabled,
+                    children: [{
+                        id: '1_group',
+                        value: 'example.org',
+                        state: ExclusionStates.Enabled,
+                        children: [
+                            {
+                                id: '1',
+                                value: 'example.org',
+                                state: ExclusionStates.Enabled,
+                                children: [],
+                            },
+                            {
+                                id: '2',
+                                value: '*.example.org',
+                                state: ExclusionStates.Enabled,
+                                children: [],
+                            },
+                        ],
+                    }],
+                },
+            ],
+        });
+    });
+
+    it('generate exclusions which are not in the service', () => {
+        const exclusions = [
+            { id: '1', hostname: 'example.org', state: ExclusionStates.Enabled },
+            { id: '2', hostname: 'example.net', state: ExclusionStates.Enabled },
+        ];
+
+        const services = {
+            services: {
+                '1_service': {
+                    id: '1_service',
+                    name: 'example',
+                    groups: [
+                        {
+                            id: '1_group',
+                            value: 'example.org',
+                        },
+                    ],
+                },
+            },
+            'example.org': '1_service',
+            '*.example.org': '1_service',
+        };
+
+        const exclusionsTree = new ExclusionsTree(exclusions, services);
+
+        exclusionsTree.generateTree();
+
+        const exclusionsData = exclusionsTree.getExclusions();
+
+        expect(exclusionsData).toEqual({
+            id: 'root',
+            value: 'root',
+            state: ExclusionStates.Enabled,
+            children: [
+                {
+                    id: '1_service',
+                    value: 'example',
+                    state: ExclusionStates.Enabled,
+                    children: [{
+                        id: '1_group',
+                        value: 'example.org',
+                        state: ExclusionStates.Enabled,
+                        children: [
+                            {
+                                id: '1',
+                                value: 'example.org',
+                                state: ExclusionStates.Enabled,
+                                children: [],
+                            },
+                        ],
+                    }],
+                },
+                {
+                    id: 'example.net',
+                    value: 'example.net',
+                    state: ExclusionStates.Enabled,
+                    children: [
+                        {
+                            id: '2',
+                            value: 'example.net',
+                            state: ExclusionStates.Enabled,
+                            children: [],
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+});
