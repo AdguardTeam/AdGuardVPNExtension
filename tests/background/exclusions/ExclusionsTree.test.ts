a4bec3bce963783662243b08419c98477985350c
Maxim Topciu <mtopciu@adguard.com>
2021-12-21 16:17:21 +0300

    AG-10618 add test for exclusion tree

--- tests/background/exclusions/ExclusionsTree.test.ts
+++ tests/background/exclusions/ExclusionsTree.test.ts
@@ -3,6 +3,8 @@ import { ServicesManager } from '../../../src/background/exclusions/services/Ser
 import { ExclusionsTree } from '../../../src/background/exclusions/ExclusionsTree';
 import { ExclusionStates, ExclusionsTypes } from '../../../src/common/exclusionsConstants';
 
+jest.mock('../../../src/lib/logger.js');
+
 describe('ExclusionsTree', () => {
     afterEach(() => {
         jest.clearAllMocks();
@@ -16,41 +18,7 @@ describe('ExclusionsTree', () => {
 
         const indexedExclusions = ExclusionsHandler.getExclusionsIndex(exclusions);
 
-        const services = {
-            aliexpress: {
-                serviceId: 'aliexpress',
-                serviceName: 'Aliexpress',
-                iconUrl: 'https://icons.adguard.org/icon?domain=aliexpress.com',
-                modifiedTime: '2021-09-14T10:23:00+0000',
-                categories: [
-                    {
-                        id: 'SHOP',
-                        name: 'Shopping',
-                    },
-                ],
-                domains: [
-                    'aliexpress.com',
-                    'aliexpress.ru',
-                ],
-            },
-            amazon: {
-                serviceId: 'amazon',
-                serviceName: 'Amazon',
-                iconUrl: 'https://icons.adguard.org/icon?domain=amazon.com',
-                modifiedTime: '2021-09-14T10:23:00+0000',
-                categories: [
-                    {
-                        id: 'SHOP',
-                        name: 'Shopping',
-                    },
-                ],
-                domains: [
-                    'amazon.com',
-                    'amazon.de',
-                    'amazon.eu',
-                ],
-            },
-        };
+        const services = {};
 
         const indexedServices = ServicesManager.getServicesIndex(services);
 
@@ -92,74 +60,99 @@ describe('ExclusionsTree', () => {
     });
 
     it('generate exclusions which are in the service', () => {
-        // const exclusions = [
-        //     { id: '1', hostname: 'example.org', state: ExclusionStates.Enabled },
-        //     { id: '2', hostname: 'example.net', state: ExclusionStates.Enabled },
-        // ];
-        //
-        // const services = {
-        //     services: {
-        //         '1_service': {
-        //             id: '1_service',
-        //             name: 'example',
-        //             groups: [
-        //                 {
-        //                     id: '1_group',
-        //                     value: 'example.org',
-        //                 },
-        //             ],
-        //         },
-        //     },
-        //     'example.org': '1_service',
-        //     '*.example.org': '1_service',
-        // };
+        const exclusions = [
+            { id: '0', hostname: 'aliexpress.com', state: ExclusionStates.Enabled },
+            { id: '1', hostname: '*.aliexpress.com', state: ExclusionStates.Enabled },
+            { id: '2', hostname: 'aliexpress.ru', state: ExclusionStates.Enabled },
+            { id: '3', hostname: '*.aliexpress.ru', state: ExclusionStates.Enabled },
+        ];
 
-        // FIXME fix test
-        expect(1).toEqual(1);
+        const indexedExclusions = ExclusionsHandler.getExclusionsIndex(exclusions);
 
-    //     const exclusionsTree = new ExclusionsTree(exclusions, services);
-    //
-    //     exclusionsTree.generateTree();
-    //
-    //     const exclusionsData = exclusionsTree.getExclusions();
-    //
-    //     expect(exclusionsData).toEqual({
-    //         id: 'root',
-    //         value: 'root',
-    //         state: ExclusionStates.Enabled,
-    //         children: [
-    //             {
-    //                 id: '1_service',
-    //                 value: 'example',
-    //                 state: ExclusionStates.Enabled,
-    //                 children: [{
-    //                     id: '1_group',
-    //                     value: 'example.org',
-    //                     state: ExclusionStates.Enabled,
-    //                     children: [
-    //                         {
-    //                             id: '1',
-    //                             value: 'example.org',
-    //                             state: ExclusionStates.Enabled,
-    //                             children: [],
-    //                         },
-    //                     ],
-    //                 }],
-    //             },
-    //             {
-    //                 id: 'example.net',
-    //                 value: 'example.net',
-    //                 state: ExclusionStates.Enabled,
-    //                 children: [
-    //                     {
-    //                         id: '2',
-    //                         value: 'example.net',
-    //                         state: ExclusionStates.Enabled,
-    //                         children: [],
-    //                     },
-    //                 ],
-    //             },
-    //         ],
-    //     });
+        const services = {
+            aliexpress: {
+                serviceId: 'aliexpress',
+                serviceName: 'Aliexpress',
+                iconUrl: 'https://icons.adguard.org/icon?domain=aliexpress.com',
+                modifiedTime: '2021-09-14T10:23:00+0000',
+                categories: [
+                    {
+                        id: 'SHOP',
+                        name: 'Shopping',
+                    },
+                ],
+                domains: [
+                    'aliexpress.com',
+                    'aliexpress.ru',
+                ],
+            },
+        };
+
+        const indexedServices = ServicesManager.getServicesIndex(services);
+
+        const exclusionsTree = new ExclusionsTree();
+
+        exclusionsTree.generateTree({
+            exclusions,
+            indexedExclusions,
+            services,
+            indexedServices,
+        });
+
+        const exclusionsData = exclusionsTree.getExclusions();
+
+        expect(exclusionsData).toEqual([
+            {
+                id: 'aliexpress',
+                value: 'Aliexpress',
+                state: ExclusionStates.Enabled,
+                type: ExclusionsTypes.Service,
+                iconUrl: 'https://icons.adguard.org/icon?domain=aliexpress.com',
+                children: [{
+                    id: 'aliexpress.com',
+                    value: 'aliexpress.com',
+                    state: ExclusionStates.Enabled,
+                    type: ExclusionsTypes.Group,
+                    children: [
+                        {
+                            id: '0',
+                            value: 'aliexpress.com',
+                            state: ExclusionStates.Enabled,
+                            type: ExclusionsTypes.Exclusion,
+                            children: [],
+                        },
+                        {
+                            id: '1',
+                            value: '*.aliexpress.com',
+                            state: ExclusionStates.Enabled,
+                            type: ExclusionsTypes.Exclusion,
+                            children: [],
+                        },
+                    ],
+                },
+                {
+                    id: 'aliexpress.ru',
+                    value: 'aliexpress.ru',
+                    state: ExclusionStates.Enabled,
+                    type: ExclusionsTypes.Group,
+                    children: [
+                        {
+                            id: '2',
+                            value: 'aliexpress.ru',
+                            state: ExclusionStates.Enabled,
+                            type: ExclusionsTypes.Exclusion,
+                            children: [],
+                        },
+                        {
+                            id: '3',
+                            value: '*.aliexpress.ru',
+                            state: ExclusionStates.Enabled,
+                            type: ExclusionsTypes.Exclusion,
+                            children: [],
+                        },
+                    ],
+                },
+                ],
+            }]);
     });
 });
