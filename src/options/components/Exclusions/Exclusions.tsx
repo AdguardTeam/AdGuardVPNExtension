39544f4ea8d6d8b3ed8063757ca081fcf04b2d4c
Anton Kaprielov <a.kaprielov@adguard.com>
2022-01-20 12:00:02 +0300

    Pull request #346: AG-12321 mode change margin
    Merge in EXTENSIONS/vpn-extension from AG-12321 to master
    
    Squashed commit of the following:
    
    commit 67a6437129803758efc7f81dda20f71d27a7c702
    Merge: 5efbe882 b8be1546
    Author: Anton <a.kaprielov@adguard.com>
    Date:   Thu Jan 20 11:55:21 2022 +0300
    
        Merge branch 'master' into AG-12321
    
    commit 5efbe88242dbf0d06eace2f9d867873b4e9dabff
    Author: Anton <a.kaprielov@adguard.com>
    Date:   Thu Jan 20 11:05:51 2022 +0300
    
        AG-12321 fix btn
    
    commit 9c3dfac9ce34887b98847d659cdf5f736fa7246b
    Author: Anton <a.kaprielov@adguard.com>
    Date:   Wed Jan 19 18:16:39 2022 +0300
    
        AG-12321 mode change margin

--- src/options/components/Exclusions/Exclusions.tsx
+++ src/options/components/Exclusions/Exclusions.tsx
@@ -46,13 +46,15 @@ export const Exclusions = observer(() => {
                 <span className="exclusions__mode__title">{mode}</span>
                 <span>: </span>
                 <span>{modeDescription}</span>
-                <button
-                    type="button"
-                    className="exclusions__mode__change-mode-btn simple-button"
-                    onClick={openModeSelectorModal}
-                >
-                    {translator.getMessage('settings_exclusion_change_mode')}
-                </button>
+                <div>
+                    <button
+                        type="button"
+                        className="exclusions__mode__change-mode-btn simple-button"
+                        onClick={openModeSelectorModal}
+                    >
+                        {translator.getMessage('settings_exclusion_change_mode')}
+                    </button>
+                </div>
             </div>
             <div>
                 <ExclusionsSearch />
