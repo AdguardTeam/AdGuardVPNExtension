808e09f683884a95d53b67fc368c8f237abe8468
tvinzz <o.labudko@adguard.com>
2022-06-10 00:17:44 +0300

    localized notifications title

--- src/background/notifications.js
+++ src/background/notifications.js
@@ -1,5 +1,6 @@
 import browser from 'webextension-polyfill';
 import { nanoid } from 'nanoid';
+import { translator } from '../common/translator';
 
 const BLANK_IMAGE_PATH = '../assets/images/blank.svg';
 
@@ -8,7 +9,7 @@ class Notifications {
         const notificationOptions = {
             type: 'basic',
             iconUrl: BLANK_IMAGE_PATH,
-            title: 'AdGuard VPN',
+            title: translator.getMessage('short_name'),
             message: options.message,
         };
         await browser.notifications.create(nanoid(), notificationOptions);
