6f06d5f1d165e43643001273a15d9b8657a87e03
Oleg Labudko <o.labudko@adguard.com>
2022-06-14 13:32:40 +0300

    Pull request: AG-14583 Remove or replace our logo from notification
    Merge in EXTENSIONS/vpn-extension from fix/AG-14583 to master
    
    Squashed commit of the following:
    
    commit c8e2d5949caa591421ce9a15e195c6391bc9c389
    Merge: 973503e1 a65ddef3
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Jun 14 13:27:24 2022 +0300
    
        Merge branch 'master' into fix/AG-14583
    
    commit 973503e15c7637fec295611431bd26838c1fbf7c
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Sun Jun 12 15:41:07 2022 +0300
    
        notifications and test to ts
    
    commit 9c51dc0fa5e5d6deb6ee71bf0b7a07cfe97c2cf4
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Fri Jun 10 00:25:24 2022 +0300
    
        fixed linter error
    
    commit fe00fda0f2fbdb63304a371d79e3b9ac2c813d26
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Fri Jun 10 00:23:35 2022 +0300
    
        added named export for notifications
    
    commit 808e09f683884a95d53b67fc368c8f237abe8468
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Fri Jun 10 00:17:44 2022 +0300
    
        localized notifications title
    
    commit 1cb054d6f765b3ad3f8ce01c10d7ba7957f96c71
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Jun 9 22:31:54 2022 +0300
    
        added blank image for notifications icon

---
+++ src/background/notifications.ts
@@ -0,0 +1,27 @@
+import browser from 'webextension-polyfill';
+import { nanoid } from 'nanoid';
+import { translator } from '../common/translator';
+
+const BLANK_IMAGE_PATH = '../assets/images/blank.svg';
+
+interface NotificationsInterface {
+    create(options: { message: string }): Promise<void>;
+}
+
+class Notifications implements NotificationsInterface {
+    /**
+     * Creates notification with provided message
+     * @param options
+     */
+    create = async (options: { message: string }): Promise<void> => {
+        const notificationOptions: browser.Notifications.CreateNotificationOptions = {
+            type: 'basic',
+            iconUrl: BLANK_IMAGE_PATH,
+            title: translator.getMessage('short_name'),
+            message: options.message,
+        };
+        await browser.notifications.create(nanoid(), notificationOptions);
+    };
+}
+
+export const notifications = new Notifications();
