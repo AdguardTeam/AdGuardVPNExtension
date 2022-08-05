b851188a6ca1f444364eaaa5b70bdc561737d9dd
tvinzz <o.labudko@adguard.com>
2022-08-05 03:40:15 +0300

    do not authenticate authenticated user and remove credentials from session storage

--- src/content-scripts/auth.js
+++ src/content-scripts/auth.js
@@ -1,7 +1,15 @@
 import browser from 'webextension-polyfill';
 import { MessageType } from '../lib/constants';
+import { messenger } from '../lib/messenger';
 
-browser.runtime.sendMessage({
-    type: MessageType.AUTHENTICATE_SOCIAL,
-    data: { queryString: window.location.href.split('#')[1] },
-});
+(async () => {
+    const isAuthenticated = await messenger.isAuthenticated();
+    if (isAuthenticated) {
+        return;
+    }
+
+    await browser.runtime.sendMessage({
+        type: MessageType.AUTHENTICATE_SOCIAL,
+        data: { queryString: window.location.href.split('#')[1] },
+    });
+})();
--- src/content-scripts/thankYouPageAuth.js
+++ src/content-scripts/thankYouPageAuth.js
@@ -2,32 +2,40 @@ import browser from 'webextension-polyfill';
 import qs from 'qs';
 
 import { MessageType } from '../lib/constants';
+import { messenger } from '../lib/messenger';
 
 const CREDENTIALS_KEY = 'credentials';
 const TOKEN_TYPE = 'bearer';
 
-const credentialsString = sessionStorage.getItem(CREDENTIALS_KEY);
+(async () => {
+    const credentialsString = sessionStorage.getItem(CREDENTIALS_KEY);
+    if (!credentialsString) {
+        return;
+    }
 
-if (credentialsString) {
+    const isAuthenticated = await messenger.isAuthenticated();
+    if (isAuthenticated) {
+        // delete credentials from sessionStorage if user is authentication already
+        sessionStorage.removeItem(CREDENTIALS_KEY);
+        return;
+    }
+
     const queryString = window.location.href.split('?')[1];
     const queryData = qs.parse(queryString);
-    const {
-        new_user: newUser,
-    } = queryData;
 
+    const { new_user: newUser } = queryData;
     const isNewUser = !!(parseInt(newUser, 10));
+
     const credentials = JSON.parse(credentialsString);
     const authCredentials = {
         ...credentials,
         tokenType: TOKEN_TYPE,
     };
 
-    (async () => {
-        await browser.runtime.sendMessage({
-            type: MessageType.AUTHENTICATE_THANKYOU_PAGE,
-            data: { authCredentials, isNewUser },
-        });
-        // delete credentials from sessionStorage after authentication
-        sessionStorage.removeItem(CREDENTIALS_KEY);
-    })();
-}
+    await browser.runtime.sendMessage({
+        type: MessageType.AUTHENTICATE_THANKYOU_PAGE,
+        data: { authCredentials, isNewUser },
+    });
+    // delete credentials from sessionStorage after authentication
+    sessionStorage.removeItem(CREDENTIALS_KEY);
+})();
