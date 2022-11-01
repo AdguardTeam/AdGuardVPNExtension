e6a602dd056a9d051cd5010a949e9d2a694d8d17
Oleg Labudko <o.labudko@adguard.com>
2022-11-01 16:49:09 +0300

    Pull request: AG-16972 Console warnings after double clicking any section
    Merge in EXTENSIONS/vpn-extension from fix/AG-16972 to master
    
    Squashed commit of the following:
    
    commit 232f5969ea2ff45fe29f2208687828a7dfae8736
    Merge: 28e1df4a c4ccc6ab
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Nov 1 20:42:20 2022 +0700
    
        Merge branch 'master' into fix/AG-16972
    
    commit 28e1df4ad9afedfeafd7922fd321446fa5e2a81f
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Sat Oct 29 04:05:35 2022 +0700
    
        added replace attribute to NavLinks in Sidebar

--- src/options/components/Sidebar/Sidebar.jsx
+++ src/options/components/Sidebar/Sidebar.jsx
@@ -16,23 +16,23 @@ export const Sidebar = observer(() => {
         <div className="sidebar">
             <div className="logo sidebar__logo" />
             <nav className="sidebar__nav" onClick={settingsStore.closeSubComponents}>
-                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/">
+                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/" replace>
                     {reactTranslator.getMessage('settings_title')}
                 </NavLink>
-                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/exclusions">
+                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/exclusions" replace>
                     {reactTranslator.getMessage('settings_exclusion_title')}
                 </NavLink>
-                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/account">
+                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/account" replace>
                     {reactTranslator.getMessage('account_title')}
                 </NavLink>
-                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/support">
+                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/support" replace>
                     {reactTranslator.getMessage('options_support_title')}
                 </NavLink>
-                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/about">
+                <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/about" replace>
                     {reactTranslator.getMessage('about_title')}
                 </NavLink>
                 {!isPremiumToken && (
-                    <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/free-gbs">
+                    <NavLink className="sidebar__link" exact activeClassName="sidebar__link--active" to="/free-gbs" replace>
                         {reactTranslator.getMessage('settings_free_gbs')}
                         <span className="sidebar__link--mark" />
                     </NavLink>
