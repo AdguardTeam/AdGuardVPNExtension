dae94d912261c4d664801c1a12c698bfca4b7cb5
tvinzz <o.labudko@adguard.com>
2023-04-27 02:56:47 +0700

    refactored tests and makeRequest method

--- tests/background/api/api.test.ts
+++ tests/background/api/api.test.ts
@@ -1,43 +1,62 @@
 import { MockServer } from 'jest-mock-server';
 import { Api } from '../../../src/background/api/Api';
-import { vpnProvider } from '../../../src/background/providers/vpnProvider';
 
-describe('Api tests', () => {
+const REQUEST_PATH = '/test-request';
+const ERROR_REQUEST_PATH = '/error-request';
+
+const testData = {
+    testValue1: 'test1',
+    testValue2: 'test2',
+};
+
+const api = new Api('');
+const getRequestUrlMock = jest.spyOn(api, 'getRequestUrl');
+
+describe('makeRequest tests', () => {
     const server = new MockServer();
 
     beforeAll(() => server.start());
+    afterEach(() => server.reset());
     afterAll(() => server.stop());
-    beforeEach(() => server.reset());
 
-    it('Test make request', async () => {
-        const BAD_REQUEST_400 = '/bad_request_400';
-        const GOOD_REQUEST_200 = '/good_request_200';
+    it('Successful request', async () => {
         server
-            .get(GOOD_REQUEST_200)
-            .mockImplementationOnce((ctx) => {
+            .get(REQUEST_PATH)
+            .mockImplementation((ctx) => {
                 ctx.status = 200;
-                ctx.body = 'response OK';
+                ctx.body = testData;
             });
 
-        server
-            .get(BAD_REQUEST_400)
-            .mockImplementationOnce((ctx) => {
-                ctx.status = 400;
-            });
-
-        // Since we did not passed any port into server constructor, server was started at random free port
         const serverOrigin = server.getURL().origin;
-        // path argument is stubbes as getRequestUrl going to be mocked each time
-        const api = new Api('');
-
-        const getRequestUrlMock = jest.spyOn(api, 'getRequestUrl');
         getRequestUrlMock.mockResolvedValue(serverOrigin);
         expect(await api.getRequestUrl('')).toBe(serverOrigin);
 
-        getRequestUrlMock.mockResolvedValue(new URL(GOOD_REQUEST_200, serverOrigin).href);
-        const data = await api.makeRequest(GOOD_REQUEST_200, {}, 'GET');
-        expect(data).toBe('response OK');
+        getRequestUrlMock.mockResolvedValue(new URL(REQUEST_PATH, serverOrigin).href);
+        const data = await api.makeRequest(REQUEST_PATH, {}, 'GET');
+        expect(data.testValue1).toBe(testData.testValue1);
+        expect(data.testValue2).toBe(testData.testValue2);
+    });
 
-        // expect(await api.makeRequest(BAD_REQUEST_400, {}, 'GET')).rejects.toThrow();
+    it('Error request', async () => {
+        server
+            .get(ERROR_REQUEST_PATH)
+            .mockImplementation((ctx) => {
+                ctx.status = 400;
+                ctx.body = JSON.stringify(testData);
+            });
+
+        const serverOrigin = server.getURL().origin;
+        getRequestUrlMock.mockResolvedValue(new URL(ERROR_REQUEST_PATH, serverOrigin).href);
+
+        try {
+            await api.makeRequest(ERROR_REQUEST_PATH, {}, 'GET');
+        } catch (e) {
+            expect(e).toBeDefined();
+            expect(e.status).toBe(400);
+
+            const responseErrorData = JSON.parse(e.message);
+            expect(responseErrorData.testValue1).toBe('test1');
+            expect(responseErrorData.testValue2).toBe('test2');
+        }
     });
 });
