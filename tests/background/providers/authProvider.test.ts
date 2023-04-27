f0e888ad2ba72f3377ee46fb14d511a19cf09cb6
Oleg Labudko <o.labudko@adguard.com>
2023-04-27 15:50:06 +0300

    Pull request: AG-21371 Use fetch instead of axios
    Merge in EXTENSIONS/vpn-extension from fix/AG-21371_1 to master
    
    Squashed commit of the following:
    
    commit 66d1120ec2ea5d53b16ca2b63305080a6aded2b7
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Apr 27 19:29:10 2023 +0700
    
        fixed type for marketingConsent
    
    commit 6b430b24ce82323dc7a5e1f2274eadf0610b19ff
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Apr 27 17:34:54 2023 +0700
    
        fixed registration request data
    
    commit dae94d912261c4d664801c1a12c698bfca4b7cb5
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Thu Apr 27 02:56:47 2023 +0700
    
        refactored tests and makeRequest method
    
    commit 2f6f5714cffaf374dc5675948c3073f851eb1d05
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Wed Apr 26 15:05:26 2023 +0300
    
        fix url resolving, add custom response data
    
    commit fa30c07f0ca3f966a4cb01ab6f3abf807086f78a
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Wed Apr 26 17:46:35 2023 +0700
    
        WIP

--- tests/background/providers/authProvider.test.ts
+++ tests/background/providers/authProvider.test.ts
@@ -1,16 +1,14 @@
-import axios from 'axios';
-
 import { authProvider } from '../../../src/background/providers/authProvider';
 import { session } from '../../__mocks__';
 // TODO: test mv3 after official switch to mv3
 import { sessionState } from '../../../src/background/stateStorage/mv2';
+import { fetchResolveMock, fetchRejectMock } from '../../__mocks__/fetchMock';
 
 jest.mock('../../../src/background/sessionStorage', () => {
     // eslint-disable-next-line global-require
     return require('../../../src/background/stateStorage/mv2');
 });
 
-jest.mock('axios');
 jest.mock('../../../src/lib/logger');
 jest.mock('../../../src/background/browserApi');
 
@@ -53,16 +51,13 @@ describe('authProvider', () => {
     };
 
     it('makes requests to the server', async () => {
-        // @ts-ignore - mock axios response
-        axios.mockResolvedValue({
+        fetchResolveMock(Promise.resolve({
             status: 200,
-            data: {
-                access_token: sampleToken.accessToken,
-                expires_in: sampleToken.expiresIn,
-                scope: sampleToken.scope,
-                token_type: sampleToken.tokenType,
-            },
-        });
+            access_token: sampleToken.accessToken,
+            expires_in: sampleToken.expiresIn,
+            scope: sampleToken.scope,
+            token_type: sampleToken.tokenType,
+        }));
 
         const response = await authProvider.getAccessToken(emptyCredentials);
 
@@ -70,8 +65,7 @@ describe('authProvider', () => {
     });
 
     it('handles network errors', async () => {
-        // @ts-ignore - mock axios response
-        axios.mockRejectedValue(new Error('Network Error'));
+        fetchRejectMock();
 
         const expectedError = new Error(JSON.stringify({ error: 'authentication_error_default' }));
 
@@ -79,17 +73,12 @@ describe('authProvider', () => {
     });
 
     it('handles errors sent from server', async () => {
-        // @ts-ignore - mock axios response
-        axios.mockRejectedValue({
+        fetchResolveMock({
             status: 401,
-            response: {
-                data: {
-                    error: 'unauthorized',
-                    error_code: 'bad_credentials',
-                    error_description: 'Sorry, unrecognized username or password',
-                },
-            },
-        });
+            error: 'unauthorized',
+            error_code: 'bad_credentials',
+            error_description: 'Sorry, unrecognized username or password',
+        }, false);
 
         const expectedError = new Error(JSON.stringify({ error: 'authentication_error_wrong_credentials' }));
 
