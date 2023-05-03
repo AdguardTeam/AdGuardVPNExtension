ac63071d47d2715551ce1975f23d530cdb70c277
tvinzz <o.labudko@adguard.com>
2023-05-03 22:49:00 +0700

    fixed mv3 implementation

--- src/background/timers/Mv3Timers.ts
+++ src/background/timers/Mv3Timers.ts
@@ -8,7 +8,10 @@ import { TimersInterface } from './AbstractTimers';
  * Alarm API used in MV3 for long timers, that could be interrupted by the service worker's death.
  */
 class Mv3Timers implements TimersInterface {
-    generateId() {
+    /**
+     * Generates ID
+     */
+    generateId(): number {
         const nanoid = customAlphabet('1234567890', 10);
         const id = nanoid();
         return parseInt(id, 10);
@@ -20,17 +23,31 @@ class Mv3Timers implements TimersInterface {
      */
     convertMsToMin = (time: number): number => time / (1000 * 60);
 
+    /**
+     * setTimeout implementation
+     * @param callback
+     * @param timeout in ms
+     */
     setTimeout = (callback: () => void, timeout: number): number => {
         const timerId = this.generateId();
-        alarmApi.createAlarm(`${timerId}`, this.convertMsToMin(timeout));
+        alarmApi.createAlarm(`${timerId}`, timeout);
         alarmApi.onAlarmFires(`${timerId}`, callback);
         return timerId;
     };
 
+    /**
+     * clearTimeout implementation
+     * @param timerId
+     */
     clearTimeout = (timerId: number): void => {
         alarmApi.clearAlarm(`${timerId}`);
     };
 
+    /**
+     * setInterval implementation
+     * @param callback
+     * @param interval in ms
+     */
     setInterval = (callback: () => void, interval: number): number => {
         const timerId = this.generateId();
         alarmApi.createPeriodicAlarm(`${timerId}`, this.convertMsToMin(interval));
@@ -38,6 +55,10 @@ class Mv3Timers implements TimersInterface {
         return timerId;
     };
 
+    /**
+     * clearInterval implementation
+     * @param intervalId
+     */
     clearInterval = (intervalId: number): void => {
         alarmApi.clearAlarm(`${intervalId}`);
     };
