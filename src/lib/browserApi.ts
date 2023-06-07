db6cb6c8c4b239f9858cf59f4d6c89d0827f5529
tvinzz <o.labudko@adguard.com>
2023-06-07 17:55:16 +0700

    removed redundants

--- src/lib/browserApi.ts
+++
@@ -1,7 +0,0 @@
-// we don't use webextension-polyfill here because of the bug
-// https://github.com/mozilla/webextension-polyfill/issues/16#issuecomment-371355255
-declare namespace browser {
-    const runtime: typeof chrome.runtime;
-}
-
-export const browserApi = chrome || browser;
