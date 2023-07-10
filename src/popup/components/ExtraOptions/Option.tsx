c148ae6444e1f0c34b8b7dd1892b4349ac2f07e4
Oleg Labudko <o.labudko@adguard.com>
2023-07-10 21:38:31 +0300

    Pull request: AG-22151 Update menu ADG VPN
    Merge in EXTENSIONS/vpn-extension from featre/AG-22151 to branch_2.1
    
    Squashed commit of the following:
    
    commit 3c5b1398c266a32438d52cbcee38f01603dcc87f
    Merge: 9481e736 de70105e
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Jul 11 01:26:55 2023 +0700
    
        Merge branch 'branch_2.1' into featre/AG-22151
    
    commit 9481e73652bdd7c6d2d78f4819b9a2c27ea74e7e
    Merge: 7c0f24fa 109c680c
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Jul 11 01:11:05 2023 +0700
    
        Merge branch 'branch_2.1' into featre/AG-22151
    
    commit 7c0f24fa37ce02f41a4dd2b052cf129e6fae5451
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Wed Jul 5 00:14:35 2023 +0700
    
        replaced color for menu item border
    
    commit 643269ae5f25ba455aebf1a5de28a3d0715c624a
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Wed Jun 28 19:32:47 2023 +0700
    
        fixed style for munu's last child
    
    commit de016aae17ecaca70877251a5a264b4fc4b82597
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Mon Jun 19 17:11:04 2023 +0700
    
        updated menu design

--- src/popup/components/ExtraOptions/Option.tsx
+++ src/popup/components/ExtraOptions/Option.tsx
@@ -1,16 +1,25 @@
 import React, { ReactNode } from 'react';
+import classnames from 'classnames';
 
 type OptionProps = {
     handler: () => void,
     text: string | ReactNode,
+    addClassName: string | null,
 };
 
-export const Option = ({ handler, text }: OptionProps) => (
-    <button
-        type="button"
-        className="button button--inline extra-options__item"
-        onClick={handler}
-    >
-        {text}
-    </button>
-);
+export const Option = ({ handler, text, addClassName }: OptionProps) => {
+    const optionClasses = classnames(
+        'button button--inline extra-options__item',
+        { [`${addClassName}`]: !!addClassName },
+    );
+
+    return (
+        <button
+            type="button"
+            className={optionClasses}
+            onClick={handler}
+        >
+            {text}
+        </button>
+    );
+};
