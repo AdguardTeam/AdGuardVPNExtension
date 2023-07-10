663dd1b3b769eb30bc520286bcd8e57b844e490e
Oleg Labudko <o.labudko@adguard.com>
2023-07-10 22:43:15 +0300

    Pull request: AG-23285 Switches become disabled until page refresh
    Merge in EXTENSIONS/vpn-extension from fix/AG-23285 to branch_2.1
    
    Squashed commit of the following:
    
    commit 481049a59a1bfb61bd6afbfa8b7b0fc5b3c2bb3c
    Merge: bffe2ef7 0a944266
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Jul 11 02:04:10 2023 +0700
    
        Merge branch 'branch_2.1' into fix/AG-23285
    
    commit bffe2ef7ef0fa8b5868446d7c142047e58f68083
    Author: tvinzz <o.labudko@adguard.com>
    Date:   Tue Jul 4 19:29:04 2023 +0700
    
        refactored wakeUpOptionsPage

--- src/background/stateStorage/helper.ts
+++ src/background/stateStorage/helper.ts
@@ -5,8 +5,10 @@ import { MessageType } from '../../lib/constants';
 /**
  * Sends the message to the option's page to update event listeners
  */
-export const wakeUpOptionsPage = () => {
-    browser.runtime.sendMessage({ type: MessageType.UPDATE_LISTENERS })
+export const wakeUpOptionsPage = async () => {
+    try {
+        await browser.runtime.sendMessage({ type: MessageType.UPDATE_LISTENERS });
+    } catch (e) {
         // ignore error of there is no options page
-        .catch(() => {});
+    }
 };
