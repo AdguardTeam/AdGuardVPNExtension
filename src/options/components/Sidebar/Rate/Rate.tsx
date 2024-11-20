1099c0e999e5aacfb52e76f97d77b1383cafd5ca
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-11-20 18:39:35 +0500

    updated rate

--- src/options/components/Sidebar/Rate/Rate.tsx
+++ src/options/components/Sidebar/Rate/Rate.tsx
@@ -1,10 +1,12 @@
-import React, { Fragment, useContext } from 'react';
+import React, { useContext } from 'react';
 import { observer } from 'mobx-react';
 
 import { FORWARDER_URL_QUERIES } from '../../../../background/config';
 import { getForwarderUrl } from '../../../../common/helpers';
 import { rootStore } from '../../../stores';
 import { reactTranslator } from '../../../../common/reactTranslator';
+
+import { RateStar } from './RateStar';
 
 import './rate.pcss';
 
@@ -22,12 +24,14 @@ export const Rate = observer(() => {
         await hideRate();
     };
 
-    const handleChange = async (e: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
-        const { value } = e.target;
+    const handleChange = async (value: number): Promise<void> => {
+        if (value < 0 || value > 5) {
+            return;
+        }
 
         await handleHideRate();
 
-        if (value && parseInt(value, 10) >= 4) {
+        if (value >= 4) {
             window.open(getForwarderUrl(forwarderDomain, FORWARDER_URL_QUERIES.OPTIONS_STORE), '_blank');
         } else {
             window.open(getForwarderUrl(forwarderDomain, FORWARDER_URL_QUERIES.FEEDBACK), '_blank');
@@ -40,30 +44,19 @@ export const Rate = observer(() => {
 
     return (
         <div className="rate">
-            <div className="rate__text">
-                {reactTranslator.getMessage('rate_description')}
-            </div>
+            <div className="rate__line" />
             <div className="rate__stars">
                 {RATING_STARS.map((star) => (
-                    <Fragment key={star}>
-                        <input
-                            type="radio"
-                            value={star}
-                            name="rating"
-                            id={`rating-${star}`}
-                            className="rate__input"
-                            onChange={handleChange}
-                        />
-                        <label
-                            htmlFor={`rating-${star}`}
-                            className="rate__star"
-                        />
-                    </Fragment>
+                    <RateStar key={star} value={star} onChange={handleChange} />
                 ))}
+            </div>
+            <div className="rate__title">
+                {/* FIXME: Translation */}
+                Enjoying AdGuard VPN?
             </div>
             <button
                 type="button"
-                className="rate__hide"
+                className="rate__hide-btn"
                 onClick={handleHideRate}
             >
                 {reactTranslator.getMessage('rate_hide')}
