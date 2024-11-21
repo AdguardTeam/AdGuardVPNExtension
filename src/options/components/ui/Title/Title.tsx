bb5b4ebe70deaddd5e244a8726d27149734e21c7
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-11-21 16:49:11 +0500

    enhanced title

--- src/options/components/ui/Title/Title.tsx
+++ src/options/components/ui/Title/Title.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
+import { Icon } from '../Icon';
+
 import './title.pcss';
 
 export interface TitleProps {
     title: React.ReactNode | string;
-
-    // FIXME: Add support
-    // eslint-disable-next-line react/no-unused-prop-types
-    subtitle?: React.ReactNode | string,
-    // eslint-disable-next-line react/no-unused-prop-types
     onClick?: () => void;
 }
 
-export function Title({ title }: TitleProps) {
+export function Title({ title, onClick }: TitleProps) {
+    const isBackTitle = !!onClick;
+
     return (
-        <div className="title">
-            <div className="title__text">{title}</div>
+        <div
+            className={classNames('title', isBackTitle && 'title--hoverable')}
+            onClick={onClick}
+        >
+            <div className="title__text">
+                <div className="title__text-start">
+                    {isBackTitle && <Icon name="arrow-down" className="title__text-start-icon" />}
+                    {title}
+                </div>
+            </div>
         </div>
     );
 }
