48b6080e3a9bf9dcdcc16ce62c49f4ef71f0dbaa
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-11-21 18:21:58 +0500

    enhanced title and controls style

--- src/options/components/ui/Title/Title.tsx
+++ src/options/components/ui/Title/Title.tsx
@@ -8,15 +8,27 @@ import './title.pcss';
 
 export interface TitleProps {
     title: React.ReactNode | string;
+    size?: 'medium' | 'large';
+    style?: React.CSSProperties;
     onClick?: () => void;
 }
 
-export function Title({ title, onClick }: TitleProps) {
+export function Title({
+    title,
+    size = 'large',
+    style,
+    onClick,
+}: TitleProps) {
     const isBackTitle = !!onClick;
 
     return (
         <div
-            className={classNames('title', isBackTitle && 'title--hoverable')}
+            className={classNames(
+                'title',
+                `title--${size}`,
+                isBackTitle && 'title--hoverable',
+            )}
+            style={style}
             onClick={onClick}
         >
             <div className="title__text">
