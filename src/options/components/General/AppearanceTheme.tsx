c857ffc8e961397ca7f33741b735287323758654
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-11-21 15:09:17 +0500

    added QuickConnect and AppearanceTheme components

---
+++ src/options/components/General/AppearanceTheme.tsx
@@ -0,0 +1,37 @@
+import { observer } from 'mobx-react';
+import React, { useContext } from 'react';
+
+import { AppearanceTheme as AppearanceThemeEnum } from '../../../common/constants';
+import { rootStore } from '../../stores';
+import { reactTranslator } from '../../../common/reactTranslator';
+import { ControlsSelect } from '../ui/Controls';
+
+export const AppearanceTheme = observer(() => {
+    const { settingsStore } = useContext(rootStore);
+
+    const handleChange = async (value: AppearanceThemeEnum): Promise<void> => {
+        await settingsStore.setAppearanceTheme(value);
+    };
+
+    return (
+        <ControlsSelect
+            title={reactTranslator.getMessage('settings_theme_label')}
+            value={settingsStore.appearanceTheme}
+            options={[
+                {
+                    value: AppearanceThemeEnum.System,
+                    title: reactTranslator.getMessage('settings_theme_system'),
+                },
+                {
+                    value: AppearanceThemeEnum.Dark,
+                    title: reactTranslator.getMessage('settings_theme_dark'),
+                },
+                {
+                    value: AppearanceThemeEnum.Light,
+                    title: reactTranslator.getMessage('settings_theme_light'),
+                },
+            ]}
+            onChange={handleChange}
+        />
+    );
+});
