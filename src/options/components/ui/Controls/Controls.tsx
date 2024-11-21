e20bb0e12bd2738cd3771e1690854b4eb9bd3a88
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-11-21 12:54:24 +0500

    added hoverable effect on controls

--- src/options/components/ui/Controls/Controls.tsx
+++ src/options/components/ui/Controls/Controls.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import './controls.pcss';
 
 export interface ControlsProps {
     title: string | React.ReactNode;
     description?: string | React.ReactNode;
     action?: React.ReactNode;
+    onClick?: () => void;
 }
 
-export function Controls({ title, description, action }: ControlsProps) {
+export function Controls({
+    title,
+    description,
+    action,
+    onClick,
+}: ControlsProps) {
     return (
-        <div className="controls">
+        <div
+            className={classNames('controls', !!onClick && 'controls--hoverable')}
+            onClick={onClick}
+        >
             <div className="controls__content">
                 <div className="controls__title">{title}</div>
                 <div className="controls__description">{description}</div>
