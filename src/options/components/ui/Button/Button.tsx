16e38dc9b4cf76d2c14266accfc936de4288babd
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-11-22 16:26:31 +0500

    added button variations

--- src/options/components/ui/Button/Button.tsx
+++ src/options/components/ui/Button/Button.tsx
@@ -1,23 +1,30 @@
 import React, { type PropsWithChildren } from 'react';
 
+import classNames from 'classnames';
+
 import { Icon } from '../Icon';
 
 import './button.pcss';
 
 export interface ButtonProps extends PropsWithChildren {
+    type?: 'button' | 'submit' | 'reset';
+    variant?: 'default' | 'ghost' | 'outline';
     beforeIconName?: string;
     onClick?: () => void;
 }
 
 export function Button({
+    type = 'button',
+    variant = 'default',
     beforeIconName,
     children,
     onClick,
 }: ButtonProps) {
     return (
         <button
-            className="button"
-            type="button"
+            className={classNames('button', `button--${variant}`)}
+            // eslint-disable-next-line react/button-has-type
+            type={type}
             onClick={onClick}
         >
             {beforeIconName && (
