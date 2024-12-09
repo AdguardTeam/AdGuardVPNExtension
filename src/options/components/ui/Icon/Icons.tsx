b64c23ca428af6c33b5be1c245c92b5f83591028
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-12-09 18:03:28 +0500

    fixed some icons

--- src/options/components/ui/Icon/Icons.tsx
+++ src/options/components/ui/Icon/Icons.tsx
@@ -51,6 +51,35 @@ export const Icons = () => (
             <path d="m5 12 6 6 8-9" />
         </symbol>
 
+        <symbol id="plus" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round">
+            <path d="M4 12H20" />
+            <path d="M12 4L12 20" />
+        </symbol>
+
+        <symbol
+            id="basket"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M7 9h10l-.76 10.08a1 1 0 0 1-1 .92H8.76a1 1 0 0 1-1-.92L7 9ZM6 6.5h12M14 6V4h-4v2M13.5 12v5-5Zm-3 0v5-5Z" />
+        </symbol>
+
+        <symbol
+            id="edit"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M4 20.5h15.983M10.778 16.435l-4.242-4.243 7.07-7.07a3 3 0 0 1 4.243 0 3 3 0 0 1 0 4.242l-7.07 7.071ZM5.121 17.85l1.415-5.658 4.242 4.243-5.657 1.414ZM16.789 9.01 13.96 6.182" />
+        </symbol>
+
         {/* Old icons, FIXME: Remove unused icons later when re-design is finished (AG-38059) */}
         <svg id="social_apple" viewBox="0 0 12 14">
             <defs>
@@ -98,11 +127,6 @@ export const Icons = () => (
             <path d="M23.79 9.952a12.022 12.022 0 01-.053 4.747 11.997 11.997 0 01-4.224 6.853h-.001l-3.424-.175-.485-3.025a7.152 7.152 0 003.078-3.652h-6.417V9.952H23.79z" fill="#518EF8" />
             <path d="M19.512 21.551v.001A11.95 11.95 0 0112 24.194c-4.57 0-8.543-2.554-10.57-6.313l3.889-3.183a7.135 7.135 0 0010.284 3.654l3.909 3.2z" fill="#28B446" />
             <path d="M19.66 2.957l-3.888 3.182a7.137 7.137 0 00-10.52 3.736l-3.91-3.2A11.998 11.998 0 0112 .194a11.95 11.95 0 017.66 2.763z" fill="#F14336" />
-        </symbol>
-
-        <symbol id="plus" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round">
-            <path d="M4 12H20" />
-            <path d="M12 4L12 20" />
         </symbol>
 
         <symbol id="check" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5" fill="none" strokeLinecap="round">
