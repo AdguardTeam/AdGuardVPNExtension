2f8fcd870d150bc9c166a3969a2383bf8ea53e5a
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-12-09 17:43:46 +0500

    added button component

---
+++ src/options/components/ui/Button/Button.tsx
@@ -0,0 +1,49 @@
+import React, { type PropsWithChildren } from 'react';
+
+import classNames from 'classnames';
+
+import { Icon } from '../Icon';
+
+import './button.pcss';
+
+export interface ButtonProps extends PropsWithChildren {
+    variant?: 'default' | 'outline' | 'ghost';
+    type?: 'button' | 'submit' | 'reset';
+    className?: string;
+    disabled?: boolean;
+    beforeIconName?: string;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export function Button({
+    variant = 'default',
+    type = 'button',
+    className,
+    disabled,
+    beforeIconName,
+    children,
+    onClick,
+}: ButtonProps) {
+    const classes = classNames(
+        'button',
+        `button--${variant}`,
+        className,
+    );
+
+    return (
+        <button
+            // eslint-disable-next-line react/button-has-type
+            type={type}
+            className={classes}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            {beforeIconName && (
+                <Icon name={beforeIconName} className="button__before-icon" />
+            )}
+            <span className="button__text">
+                {children}
+            </span>
+        </button>
+    );
+}
