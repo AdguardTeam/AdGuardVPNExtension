6d7b99af4bb32f62eea599e344e39412e71dd506
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-12-10 14:34:26 +0500

    updated about page

--- src/options/components/About/About.tsx
+++ src/options/components/About/About.tsx
@@ -2,63 +2,77 @@ import React, { useContext } from 'react';
 import { observer } from 'mobx-react';
 
 import { FORWARDER_URL_QUERIES } from '../../../background/config';
+import { translator } from '../../../common/translator';
+import { getForwarderUrl } from '../../../common/helpers';
 import { rootStore } from '../../stores';
 import { Title } from '../ui/Title';
-import { getForwarderUrl } from '../../../common/helpers';
-import { reactTranslator } from '../../../common/reactTranslator';
 
 import './about.pcss';
 
 export const About = observer(() => {
     const { settingsStore } = useContext(rootStore);
-
     const { forwarderDomain } = settingsStore;
 
     const websiteUrl = getForwarderUrl(forwarderDomain, FORWARDER_URL_QUERIES.WEBSITE);
     const eulaUrl = getForwarderUrl(forwarderDomain, FORWARDER_URL_QUERIES.EULA);
     const privacyUrl = getForwarderUrl(forwarderDomain, FORWARDER_URL_QUERIES.PRIVACY);
 
-    const aboutVersionStr = `${reactTranslator.getMessage('account_version')} ${settingsStore.appVersion}`;
+    const aboutVersionStr = `${translator.getMessage('account_version')} ${settingsStore.appVersion}`;
+
+    const currentYear = new Date().getFullYear();
+    const copyRightText = `© 2009-${currentYear} AdGuard Software Ltd.`;
 
     return (
         <>
-            <Title title={reactTranslator.getMessage('about_title')} />
-            <div className="about">
-                <div className="about__header">
-                    <div className="about__title">
-                        {reactTranslator.getMessage('short_name')}
-                    </div>
-                    <div className="about__version">
-                        {aboutVersionStr}
-                    </div>
+            <Title title={translator.getMessage('about_title')} />
+            <div className="about__info">
+                <div className="about__info-name">
+                    {translator.getMessage('short_name')}
                 </div>
-                <nav className="about__nav">
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={websiteUrl}
-                        className="about__link"
-                    >
-                        {reactTranslator.getMessage('official_website')}
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={eulaUrl}
-                        className="about__link"
-                    >
-                        {reactTranslator.getMessage('eula')}
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={privacyUrl}
-                        className="about__link"
-                    >
-                        {reactTranslator.getMessage('privacy_policy')}
-                    </a>
-                </nav>
+                <div className="about__info-version">
+                    {aboutVersionStr}
+                </div>
             </div>
+            <div className="about__copyright">
+                <div className="about__copyright-item">
+                    {copyRightText}
+                </div>
+                <div className="about__copyright-item">
+                    {/* FIXME: Add translation text */}
+                    {/* {translator.getMessage('all_rights_reserved')} */}
+                    All rights reserved.
+                </div>
+            </div>
+            <a
+                href={websiteUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button has-tab-focus button--ghost about__link"
+            >
+                <span className="button__text">
+                    {translator.getMessage('official_website')}
+                </span>
+            </a>
+            <a
+                href={eulaUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button has-tab-focus button--ghost about__link"
+            >
+                <span className="button__text">
+                    {translator.getMessage('eula')}
+                </span>
+            </a>
+            <a
+                href={privacyUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button has-tab-focus button--ghost about__link"
+            >
+                <span className="button__text">
+                    {translator.getMessage('privacy_policy')}
+                </span>
+            </a>
         </>
     );
 });
