a0e252fd258533672e151dfb7b4d0de656874f05
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-12-10 11:01:15 +0500

    updated notifications

--- src/options/components/ui/Notifications/Notifications.tsx
+++ src/options/components/ui/Notifications/Notifications.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { observer } from 'mobx-react';
 
 import { rootStore } from '../../../stores';
 
-import { NotificationUi } from './NotificationUi';
+import { NotificationItem } from './NotificationItem';
 
 import './notifications.pcss';
 
@@ -11,11 +11,23 @@ export const Notifications = observer(() => {
     const { notificationsStore } = useContext(rootStore);
     const { notifications } = notificationsStore;
 
+    const ref = useRef<HTMLDivElement>(null);
+
+    const firstThreeNotifications = notifications.slice(0, 3);
+
+    const handleClose = (notificationId: string) => {
+        notificationsStore.removeNotification(notificationId);
+    };
+
     return (
-        <div className="notifications">
-            {notifications.map((notification) => {
-                return <NotificationUi key={notification.id} notification={notification} />;
-            })}
+        <div ref={ref} className="notifications">
+            {firstThreeNotifications.map((notification) => (
+                <NotificationItem
+                    key={notification.id}
+                    value={notification}
+                    onClose={handleClose}
+                />
+            ))}
         </div>
     );
 });
