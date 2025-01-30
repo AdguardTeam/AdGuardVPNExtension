ce9632813624a75cfedf768fc41d0852c9b19bba
Ruslan Kurbanali <r.kurbanali@adguard.com>
2025-01-30 08:41:36 +0300

    AG-38264 Add .crx files to artifacts
    Squashed commit of the following:
    
    commit e971af702e4ea398be0833a621dab8471c99b0ad
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Wed Jan 29 11:13:23 2025 +0500
    
        updated README
    
    commit c473a5b3a93c63cb2759d251383b4e2c658f5d4e
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Wed Jan 29 10:36:58 2025 +0500
    
        updated readme
    
    commit ab0cc32d551d2705536b84aee735dbcfe23b5955
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Wed Jan 29 10:27:43 2025 +0500
    
        added test certificate
    
    commit 9209065bd11eca70670ed6762b566deeda4d4239
    Merge: 8bf9d0e8 cb3f1abb
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Wed Jan 29 10:08:25 2025 +0500
    
        Merge branch 'master' into feature/AG-38264
    
    commit 8bf9d0e8f23038ddfafabbc2aee6c3568609d085
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Wed Jan 29 10:07:47 2025 +0500
    
        reverted some changes
    
    commit d980bce1e7e0bb19c0df95ac60f1f0e8312148e8
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Tue Jan 28 14:27:07 2025 +0500
    
        updated readme
    
    commit d2eaf28085f5f5b41e988a3cd51120a942884ad7
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Tue Jan 28 12:07:32 2025 +0500
    
        review changes
    
    commit 89d6040ade49f47fcda4117f1e1a5e76a0460bd6
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 16:54:07 2025 +0500
    
        fixed not created folder bug
    
    commit 71bc1b4b9d9cf69bf3199efdeb00ae815ef8c8b5
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 16:25:40 2025 +0500
    
        added comment
    
    commit 49d8ff3ce5adf35c866a3bf2e560507301e025b2
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 16:24:48 2025 +0500
    
        added generating key for dev builds
    
    commit edda857d894392a174cea8a8e71366fdcb9dd2a5
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 13:33:32 2025 +0500
    
        updated readme
    
    commit 1b3b5186aa423ffeae2da37687749b9634c89333
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 13:25:43 2025 +0500
    
        updated readme
    
    commit ffca8c175d525b6e8c700c472abbd89c99c52415
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 13:15:21 2025 +0500
    
        fix tests spec stage env
    
    commit 902d2287cba4a969ecf4cee323d21262fe28b1a6
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Jan 27 13:01:34 2025 +0500
    
        minor changes
    
    commit 1cb6e5e6be4039091f3f59d02968585eda59be0e
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Fri Jan 24 19:03:20 2025 +0500
    
        minor fixes
    
    commit 8473dfa707713b4275d3a8e1f13d73c2ed7b6be1
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Fri Jan 24 18:43:10 2025 +0500
    
        updated readme
    
    commit 4019c4c64d90d61878d4c16fcb40527ac2acead0
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Fri Jan 24 18:43:03 2025 +0500
    
        updated build plans
    
    commit c0d848c514834f324d6be21159895b2e7c10204b
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Fri Jan 24 18:18:29 2025 +0500
    
        updated crx build

--- tasks/consts.ts
+++ tasks/consts.ts
@@ -53,30 +53,31 @@ export const IS_BETA = BUILD_ENV ? BUILD_ENV === Env.Beta : false;
 // Build output path
 export const BUILD_PATH = '../build';
 export const CRX_NAME = 'chrome.crx';
-export const CRX_MV3_NAME = 'chrome-mv3.crx';
+export const CRX_PROD_NAME = 'chrome-prod.crx';
 export const XPI_NAME = 'firefox.xpi';
 export const CHROME_UPDATER_FILENAME = 'update.xml';
 export const FIREFOX_UPDATER_FILENAME = 'update.json';
 export const MANIFEST_NAME = 'manifest.json';
 
-export const MV3 = 'mv3';
-
 // Chrome CRX certificate paths
 export const CERTIFICATE_PATHS = {
-    beta: './private/AdguardVPN/certificate-beta.pem',
-    release: './private/AdguardVPN/certificate-release.pem',
+    [Env.Dev]: './tests/certificate-test.pem',
+    [Env.Beta]: './private/AdguardVPN/certificate-beta.pem',
+    [Env.Release]: './private/AdguardVPN/certificate-release.pem',
 };
 
-export const deployPath = process.env.BUILD_ENV || Env.Dev;
+export const DEPLOY_PATH = process.env.BUILD_ENV || Env.Dev;
+
+export const UPDATE_BASE_URL = 'https://static.adtidy.net/extensions/adguardvpn';
 
 // Update manifest URL for the Chrome extension
-export const CHROME_UPDATE_URL = `https://static.adtidy.net/extensions/adguardvpn/${deployPath}/${CHROME_UPDATER_FILENAME}`;
+export const CHROME_UPDATE_URL = `${UPDATE_BASE_URL}/${DEPLOY_PATH}/${CHROME_UPDATER_FILENAME}`;
 
 // Update manifest URL for the Firefox add-on
-export const FIREFOX_UPDATE_URL = `https://static.adtidy.net/extensions/adguardvpn/${deployPath}/${FIREFOX_UPDATER_FILENAME}`;
+export const FIREFOX_UPDATE_URL = `${UPDATE_BASE_URL}/${DEPLOY_PATH}/${FIREFOX_UPDATER_FILENAME}`;
 
 // Path to the Chrome CRX (that we'll add to the update manifest)
-export const CHROME_UPDATE_CRX = `https://static.adtidy.net/extensions/adguardvpn/${deployPath}/${CRX_NAME}`;
+export const CHROME_UPDATE_CRX = `${UPDATE_BASE_URL}/${DEPLOY_PATH}/${CRX_NAME}`;
 
 // Path to the Firefox XPI (that we'll add to the update manifest)
-export const FIREFOX_UPDATE_XPI = `https://static.adtidy.net/extensions/adguardvpn/${deployPath}/${XPI_NAME}`;
+export const FIREFOX_UPDATE_XPI = `${UPDATE_BASE_URL}/${DEPLOY_PATH}/${XPI_NAME}`;
--- tasks/crx.ts
+++ tasks/crx.ts
@@ -6,11 +6,10 @@ import Crx from 'crx';
 import chalk from 'chalk';
 import { program } from 'commander';
 
-const { log, error } = console;
+import packageJson from '../package.json';
 
-const packageJson = require('../package.json');
-
-const {
+import { updateManifest } from './helpers';
+import {
     CHROME_UPDATE_URL,
     MANIFEST_NAME,
     Browser,
@@ -20,26 +19,48 @@ const {
     CHROME_UPDATE_CRX,
     CHROME_UPDATER_FILENAME,
     CRX_NAME,
-} = require('./consts');
-const { updateManifest } = require('./helpers');
+    CRX_PROD_NAME,
+    IS_DEV,
+    STAGE_ENV,
+    StageEnv,
+    type Env,
+} from './consts';
+
+const { log, error } = console;
 
 const { BUILD_ENV } = process.env;
 const { outputPath } = BUILD_ENV_MAP[BUILD_ENV as string];
 
 const WRITE_PATH = path.resolve(__dirname, BUILD_PATH, outputPath);
 
-const getPrivateKey = async () => {
-    const certificatePath = CERTIFICATE_PATHS[BUILD_ENV as string];
+/**
+ * Retrieves certificate private key depending on the environment
+ *
+ * @param crxName Name of the crx file (used for logging)
+ * @returns Buffer of the private key
+ *
+ * @throws Error if the certificate is not found
+ */
+const getPrivateKey = async (crxName: string) => {
+    const certificatePath = CERTIFICATE_PATHS[BUILD_ENV as Env];
     try {
         const privateKey = await fs.readFile(certificatePath);
         log(chalk.greenBright(`\nThe certificate is read from ${certificatePath}\n`));
         return privateKey;
     } catch (e: any) {
-        error(chalk.redBright(`Can not create ${CRX_NAME} - the valid certificate is not found in ${certificatePath} - ${e.message}\n`));
+        error(chalk.redBright(`Can not create ${crxName} - the valid certificate is not found in ${certificatePath} - ${e.message}\n`));
         throw e;
     }
 };
 
+/**
+ * Creates crx file from the loaded file
+ *
+ * @param loadedFile Loaded crx file
+ * @param crxName Name of the crx file
+ *
+ * @throws Error if the crx file can not be created
+ */
 // TODO: remove any when crx will support types
 const createCrx = async (loadedFile: any, crxName: string) => {
     try {
@@ -53,40 +74,73 @@ const createCrx = async (loadedFile: any, crxName: string) => {
     }
 };
 
+/**
+ * Creates update XML file for the extension manifest
+ *
+ * @param crx Instance of the Crx package
+ * @param updateName Name of the update file
+ *
+ * @throws Error if the update file can not be created
+ */
 // TODO: remove any when crx will support types
-const createXml = async (crx: any) => {
-    const xmlBuffer = await crx.generateUpdateXML();
-    const writeXmlPath = path.resolve(WRITE_PATH, CHROME_UPDATER_FILENAME);
-    await fs.writeFile(writeXmlPath, xmlBuffer);
-    log(chalk.greenBright(`${CHROME_UPDATER_FILENAME} saved to ${WRITE_PATH}\n`));
+const createXml = async (crx: any, updateName: string) => {
+    try {
+        const xmlBuffer = await crx.generateUpdateXML();
+        const writeXmlPath = path.resolve(WRITE_PATH, updateName);
+        await fs.writeFile(writeXmlPath, xmlBuffer);
+        log(chalk.greenBright(`${updateName} saved to ${WRITE_PATH}\n`));
+    } catch (e: any) {
+        error(chalk.redBright(`Error: Can not create ${updateName} - ${e.message}\n`));
+        throw e;
+    }
 };
 
+/**
+ * Packs extension to crx file for chrome browser with manifest version 3.
+ *
+ * Output files:
+ * - chrome.crx and updated.xml - beta and release environments
+ * - chrome.crx - dev environment with test stage environment
+ * - chrome-prod.crx - dev environment with prod stage environment
+ */
 const generateChromeFiles = async () => {
     const loadPath = path.resolve(WRITE_PATH, Browser.Chrome);
     const manifestPath = path.resolve(loadPath, MANIFEST_NAME);
 
+    let crxFileName = CRX_NAME;
+    if (IS_DEV && STAGE_ENV === StageEnv.Prod) {
+        crxFileName = CRX_PROD_NAME;
+    }
+
     try {
         const chromeManifest = await fs.readFile(manifestPath);
-        const PRIVATE_KEY = await getPrivateKey();
+        const privateKey = await getPrivateKey(crxFileName);
 
         const crx = new Crx({
             codebase: CHROME_UPDATE_CRX,
-            privateKey: PRIVATE_KEY,
+            privateKey,
             publicKey: packageJson.name,
         });
 
-        // Add to the chrome manifest `update_url` property
-        // which is to be present while creating the crx file
-        const updatedManifest = updateManifest(chromeManifest, { update_url: CHROME_UPDATE_URL });
-        await fs.writeFile(manifestPath, updatedManifest);
+        if (IS_DEV) {
+            // for dev environment we just create crx file
+            const loadedFile = await crx.load(loadPath);
+            await createCrx(loadedFile, crxFileName);
+        } else {
+            // for beta and release environments we add the update_url property to the manifest
+            // Add to the chrome manifest `update_url` property
+            // which is to be present while creating the crx file
+            const updatedManifest = updateManifest(chromeManifest, { update_url: CHROME_UPDATE_URL });
+            await fs.writeFile(manifestPath, updatedManifest);
 
-        const loadedFile = await crx.load(loadPath);
-        await createCrx(loadedFile, CRX_NAME);
-        await createXml(crx);
+            const loadedFile = await crx.load(loadPath);
+            await createCrx(loadedFile, crxFileName);
+            await createXml(crx, CHROME_UPDATER_FILENAME);
 
-        // Delete from the chrome manifest `update_url` property
-        // after the crx file has been created - reset the manifest
-        await fs.writeFile(manifestPath, chromeManifest);
+            // Delete from the chrome manifest `update_url` property
+            // after the crx file has been created - reset the manifest
+            await fs.writeFile(manifestPath, chromeManifest);
+        }
     } catch (e: any) {
         error(chalk.redBright(e.message));
         error(e);
@@ -97,9 +151,7 @@ const generateChromeFiles = async () => {
 };
 
 program
-    .description('Builds for manifest version 3')
-    .action(() => {});
+    .description('Packs extension to crx file for chrome browser with manifest version 3')
+    .action(generateChromeFiles);
 
 program.parse(process.argv);
-
-generateChromeFiles();
