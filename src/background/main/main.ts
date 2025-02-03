e3a23aadad54e06314c2b8dc5fbf45c5592421d8
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-02-03 10:47:48 +0500

    small changes

--- src/background/main/main.ts
+++ src/background/main/main.ts
@@ -114,7 +114,7 @@ const asyncInitModules = async (): Promise<void> => {
         await auth.init();
         await locationsService.init();
         await settings.init();
-        await telemetry.initState();
+        telemetry.initState();
         // the updateBrowserActionItems method is called after settings.init() because it uses settings
         await contextMenu.updateBrowserActionItems();
         // the checkAndSwitchStorage is called after settings.init() because it uses settings
