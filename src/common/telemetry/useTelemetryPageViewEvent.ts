f02f45892da8173b3100fabbf8ea639bae3f8a26
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-02-05 11:56:01 +0500

    updated jsdoc

--- src/common/telemetry/useTelemetryPageViewEvent.ts
+++ src/common/telemetry/useTelemetryPageViewEvent.ts
@@ -4,16 +4,16 @@ import { type TelemetryScreenName } from '../../background/telemetry';
 import { messenger } from '../messenger';
 
 /**
- * Hook that sends a page view telemetry event when the component is mounted.
- * Event will be fired only when `condition` is `true`, this can be useful in 2 cases:
- * 1) When specified `screenName` is overlapped by another screen, and you need to send telemetry
+ * Hook that sends a page view telemetry event when `condition` is `true`.
+ *
+ * `condition` param can be used in 3 cases:
+ * 1) Default behavior is to send telemetry event when the component is mounted.
+ * 2) When specified `screenName` is overlapped by another screen, and you need to send telemetry
  *    event when another screen is closed. For example, when you have a dialog that rendered
  *   on top of `screenName`, and you want to send telemetry event when dialog gets closed.
- * 2) When specified `screenName` is rendered conditionally, and you want to send telemetry event,
+ * 3) When specified `screenName` is rendered conditionally, and you want to send telemetry event,
  *    only when the `screenName` is rendered. For example, dialog that is rendered only when
  *    `condition` is `true`.
- *
- * Default behavior is to send telemetry event when the component is mounted.
  *
  * @param screenName Name of the screen to be logged in telemetry.
  * @param condition Condition when the telemetry event should be sent.
