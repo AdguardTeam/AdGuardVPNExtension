5717b65f07583e37125be4af8b753a0a858b1e73
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-04-25 10:40:03 +0500

    updated tests

--- tests/__setups__/chrome.ts
+++ tests/__setups__/chrome.ts
@@ -1,3 +1,5 @@
+import { sessionStorageMock } from '../__mocks__/sessionStorageMock';
+
 const chrome = require('sinon-chrome');
 
 if (!chrome.runtime.id) {
@@ -14,30 +16,7 @@ declare global {
 
 global.chrome = chrome;
 
-global.chrome.storage.session = {
-    __storage: {},
-    set: async (setter: Record<string, any>): Promise<void> => {
-        Object.keys(setter).forEach((key) => {
-            // @ts-expect-error
-            global.chrome.storage.session.__storage[key] = setter[key];
-        });
-    },
-    // @ts-expect-error - partially implemented
-    get: async (key: string | null): Promise<Record<string, any>> => {
-        if (key === null) {
-            // @ts-expect-error
-            return global.chrome.storage.session.__storage;
-        }
-
-        return {
-            // @ts-expect-error
-            [key]: global.chrome.storage.session.__storage[key],
-        };
-    },
-    clear: async (): Promise<void> => {
-        // @ts-expect-error
-        global.chrome.storage.session.__storage = {};
-    },
-};
+// @ts-expect-error Partially mocked
+global.chrome.storage.session = sessionStorageMock;
 
 export {};
