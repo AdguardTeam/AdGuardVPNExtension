9870e62bf63fc70abc4a0e1ccf9986b33fa25dc2
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-05-15 18:03:37 +0500

    exported connectivity event types

--- src/background/connectivity/index.ts
+++ src/background/connectivity/index.ts
@@ -1,6 +1,12 @@
 // eslint-disable-next-line import/no-cycle
 import { endpointConnectivity } from './endpointConnectivity';
 
+export {
+    type ConnectivityInfoMsgRefreshTokensEvent,
+    type ConnectivityInfoMsgStatsEvent,
+    type ConnectivityInfoMsgEvent,
+} from './endpointConnectivity/connectivityInfoMsg';
+
 export const connectivity = {
     endpointConnectivity,
 };
