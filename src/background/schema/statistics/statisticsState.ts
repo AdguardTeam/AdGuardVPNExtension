19904dee83ecaaa61ad46c6459044ae53736bba1
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-05-23 10:53:16 +0500

    added jsdoc

--- src/background/schema/statistics/statisticsState.ts
+++ src/background/schema/statistics/statisticsState.ts
@@ -1,5 +1,8 @@
 import zod from 'zod';
 
+/**
+ * Schema for the statistics state.
+ */
 export const statisticsStateScheme = zod.object({
     isPremiumToken: zod.boolean(),
     locationId: zod.string().nullable(),
@@ -7,8 +10,14 @@ export const statisticsStateScheme = zod.object({
     durationIntervalId: zod.number().nullable(),
 });
 
+/**
+ * Type for the statistics state.
+ */
 export type StatisticsState = zod.infer<typeof statisticsStateScheme>;
 
+/**
+ * Default values for the statistics state.
+ */
 export const STATISTICS_STATE_DEFAULTS: StatisticsState = {
     isPremiumToken: false,
     locationId: null,
