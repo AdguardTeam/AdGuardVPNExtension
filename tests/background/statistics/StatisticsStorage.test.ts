8a80bc6af272e28e823664c29c39c3163ef238a8
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-05-23 12:57:13 +0500

    separated utils test

--- tests/background/statistics/StatisticsStorage.test.ts
+++ tests/background/statistics/StatisticsStorage.test.ts
@@ -4,7 +4,6 @@ import {
     type StatisticsDurationTracker,
     type StatisticsLocationStorage,
 } from '../../../src/background/statistics/statisticsTypes';
-import { dateToKey, keyToDate } from '../../../src/background/statistics/utils';
 import { ONE_DAY_MS, ONE_HOUR_MS } from '../../../src/common/constants';
 
 jest.mock('../../../src/common/logger');
@@ -993,34 +992,5 @@ describe('StatisticsStorage', () => {
 
             storageMock.get.mockReset();
         });
-    });
-
-    it('should convert date to key and key to date properly', () => {
-        // date to key - hourly
-        expect(dateToKey(true)).toBe('2025-10-01-10');
-        expect(dateToKey(true, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31-23');
-        expect(dateToKey(true, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01-00');
-
-        // date to key - daily
-        expect(dateToKey(false)).toBe('2025-10-01');
-        expect(dateToKey(false, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31');
-        expect(dateToKey(false, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01');
-
-        // key to date - hourly
-        expect(keyToDate('2025-10-01-10')).toEqual(new Date('2025-10-01T10:00:00Z'));
-        expect(keyToDate('2025-12-31-23')).toEqual(new Date('2025-12-31T23:00:00Z'));
-        expect(keyToDate('2025-01-01-00')).toEqual(new Date('2025-01-01T00:00:00Z'));
-
-        // key to date - daily
-        expect(keyToDate('2025-10-01')).toEqual(new Date('2025-10-01T00:00:00Z'));
-        expect(keyToDate('2025-12-31')).toEqual(new Date('2025-12-31T00:00:00Z'));
-        expect(keyToDate('2025-01-01')).toEqual(new Date('2025-01-01T00:00:00Z'));
-
-        // key to date - invalid
-        expect(keyToDate('invalid')).toBe(null);
-        expect(keyToDate('2025-10-01-10-00')).toBe(null);
-        expect(keyToDate('2025-10-test')).toBe(null);
-        expect(keyToDate('2025-test-01')).toBe(null);
-        expect(keyToDate('test-01-01')).toBe(null);
     });
 });
