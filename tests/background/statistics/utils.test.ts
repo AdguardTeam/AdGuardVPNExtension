bd0589a6ed52283dad4ad33c668c5459e1c50dc1
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-05-28 19:15:20 +0500

    fixed date tests

--- tests/background/statistics/utils.test.ts
+++ tests/background/statistics/utils.test.ts
@@ -1,5 +1,13 @@
 import { dateToKey, keyToDate } from '../../../src/background/statistics/utils';
 
+beforeAll(() => {
+    jest.useFakeTimers('modern').setSystemTime(new Date('2025-10-01T10:25:10Z'));
+});
+
+afterAll(() => {
+    jest.useRealTimers();
+});
+
 describe('Statistics utils', () => {
     describe('dateToKey', () => {
         jest.useFakeTimers('modern').setSystemTime(new Date('2025-10-01T10:25:10Z'));
@@ -15,13 +23,9 @@ describe('Statistics utils', () => {
             expect(dateToKey(true, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31-23');
             expect(dateToKey(true, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01-00');
         });
-
-        jest.useRealTimers();
     });
 
     describe('keyToDate', () => {
-        jest.useFakeTimers('modern').setSystemTime(new Date('2025-10-01T10:25:10Z'));
-
         it('should convert key to date', () => {
             expect(keyToDate('2025-10-01')).toEqual(new Date('2025-10-01T00:00:00Z'));
             expect(keyToDate('2025-12-31')).toEqual(new Date('2025-12-31T00:00:00Z'));
@@ -41,7 +45,5 @@ describe('Statistics utils', () => {
             expect(keyToDate('2025-test-01')).toBe(null);
             expect(keyToDate('test-01-01')).toBe(null);
         });
-
-        jest.useRealTimers();
     });
 });
