b7e32e35fde589d862188306d7106058465a06fa
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-05-28 19:07:37 +0500

    refactor utils tests

--- tests/background/statistics/utils.test.ts
+++ tests/background/statistics/utils.test.ts
@@ -1,35 +1,46 @@
 import { dateToKey, keyToDate } from '../../../src/background/statistics/utils';
 
 describe('Statistics utils', () => {
-    it('should convert date to key and key to date properly', () => {
+    describe('dateToKey', () => {
         jest.useFakeTimers('modern').setSystemTime(new Date('2025-10-01T10:25:10Z'));
 
-        // date to key - hourly
-        expect(dateToKey(true)).toBe('2025-10-01-10');
-        expect(dateToKey(true, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31-23');
-        expect(dateToKey(true, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01-00');
+        it('should convert date to key', () => {
+            expect(dateToKey(false)).toBe('2025-10-01');
+            expect(dateToKey(false, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31');
+            expect(dateToKey(false, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01');
+        });
 
-        // date to key - daily
-        expect(dateToKey(false)).toBe('2025-10-01');
-        expect(dateToKey(false, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31');
-        expect(dateToKey(false, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01');
+        it('should convert date to key with hours', () => {
+            expect(dateToKey(true)).toBe('2025-10-01-10');
+            expect(dateToKey(true, new Date('2025-12-31T23:59:59Z'))).toBe('2025-12-31-23');
+            expect(dateToKey(true, new Date('2025-01-01T00:00:00Z'))).toBe('2025-01-01-00');
+        });
 
-        // key to date - hourly
-        expect(keyToDate('2025-10-01-10')).toEqual(new Date('2025-10-01T10:00:00Z'));
-        expect(keyToDate('2025-12-31-23')).toEqual(new Date('2025-12-31T23:00:00Z'));
-        expect(keyToDate('2025-01-01-00')).toEqual(new Date('2025-01-01T00:00:00Z'));
+        jest.useRealTimers();
+    });
 
-        // key to date - daily
-        expect(keyToDate('2025-10-01')).toEqual(new Date('2025-10-01T00:00:00Z'));
-        expect(keyToDate('2025-12-31')).toEqual(new Date('2025-12-31T00:00:00Z'));
-        expect(keyToDate('2025-01-01')).toEqual(new Date('2025-01-01T00:00:00Z'));
+    describe('keyToDate', () => {
+        jest.useFakeTimers('modern').setSystemTime(new Date('2025-10-01T10:25:10Z'));
 
-        // key to date - invalid
-        expect(keyToDate('invalid')).toBe(null);
-        expect(keyToDate('2025-10-01-10-00')).toBe(null);
-        expect(keyToDate('2025-10-test')).toBe(null);
-        expect(keyToDate('2025-test-01')).toBe(null);
-        expect(keyToDate('test-01-01')).toBe(null);
+        it('should convert key to date', () => {
+            expect(keyToDate('2025-10-01')).toEqual(new Date('2025-10-01T00:00:00Z'));
+            expect(keyToDate('2025-12-31')).toEqual(new Date('2025-12-31T00:00:00Z'));
+            expect(keyToDate('2025-01-01')).toEqual(new Date('2025-01-01T00:00:00Z'));
+        });
+
+        it('should convert date to key with hours', () => {
+            expect(keyToDate('2025-10-01-10')).toEqual(new Date('2025-10-01T10:00:00Z'));
+            expect(keyToDate('2025-12-31-23')).toEqual(new Date('2025-12-31T23:00:00Z'));
+            expect(keyToDate('2025-01-01-00')).toEqual(new Date('2025-01-01T00:00:00Z'));
+        });
+
+        it('should return null for invalid keys', () => {
+            expect(keyToDate('invalid')).toBe(null);
+            expect(keyToDate('2025-10-01-10-00')).toBe(null);
+            expect(keyToDate('2025-10-test')).toBe(null);
+            expect(keyToDate('2025-test-01')).toBe(null);
+            expect(keyToDate('test-01-01')).toBe(null);
+        });
 
         jest.useRealTimers();
     });
